4e2ba5387c64228e04d4707f1f2c5438574d0793
mean <fixounet@free.fr>
2021-06-08 08:00:53 +0200

    add mcp demo project (incomplete)

---
+++ demoProject/mcp23017/hello.cpp
@@ -0,0 +1,40 @@
+//
+#include "Arduino.h"
+#include "lnI2C.h"
+#include "lnMcp23017.h"
+// green = PA1, blue = PA2, RED PC13
+#define LED     PA2
+#define LEDRED  PC13
+#define SPEED   (400*1000)
+
+/**
+ * 
+ */
+void setup()
+{
+   
+}
+
+/**
+ * 
+ */
+void loop()
+{
+  Logger("MCP23017 I2C demo...\n");
+  
+  lnI2C *i2c=new lnI2C(1,400000);
+  i2c->begin(0x20);
+  
+  lnMcp23017 *mcp=new lnMcp23017(i2c,PB5,0x20);
+  mcp->begin();
+  while(1)
+  {
+    
+      xDelay(500);
+  }
+}
+
+
+// EOF
+
+
