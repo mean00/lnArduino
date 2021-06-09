bfd83ced3de0c45b710e27eb2aaa7451b93a8da0
mean <fixounet@free.fr>
2021-06-09 19:27:35 +0200

    nuclei

--- freeRTOS.cmake
+++ freeRTOS.cmake
@@ -6,11 +6,16 @@ FOREACH(common croutine.c  event_groups.c  list.c  queue.c  stream_buffer.c  tas
 ENDFOREACH(common croutine.c  event_groups.c  list.c  queue.c  stream_buffer.c  tasks.c  timers.c)
 
 #SET(PORT ${FOS}/portable/GCC/RISC-V)
-SET(PORT ${ARDUINO_GD32_FREERTOS}/freeRTOS_extension/N200)
-
+IF(1)
+    SET(PORT ${ARDUINO_GD32_FREERTOS}/freeRTOS_extension/N200_nuclei)
+    SET(N200  ${PORT}/port.c ${PORT}/portasm.S )
+ELSE()
+    SET(PORT ${ARDUINO_GD32_FREERTOS}/freeRTOS_extension/N200)
+    SET(N200  ${PORT}/port.c ${PORT}/portASM.S )
+ENDIF()
 SET(MEM ${FOS}/portable/MemMang/heap_4.c)
 
-ADD_LIBRARY( FreeRTOS STATIC ${CMN} ${PORT}/port.c ${PORT}/portASM.S ${MEM})
+ADD_LIBRARY( FreeRTOS STATIC ${CMN} ${N200} ${MEM})
 target_include_directories(FreeRTOS PRIVATE ${FOS}/..)
 target_include_directories(FreeRTOS PRIVATE ${FOS}/../legacy/include)
 target_include_directories(FreeRTOS PRIVATE ${FOS}/include)
---
+++ freeRTOS_extension/N200_nuclei/README.md
@@ -0,0 +1,4 @@
+Mean00 : This is a modified version of the freeRTOS port coming from nuclei sdk
+License is MIT (?)
+
+https://github.com/Nuclei-Software/nuclei-sdk
