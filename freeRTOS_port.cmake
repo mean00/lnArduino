31e3abab9d223bd941b47e3ee9dfbc830d54ce37
mean <fixounet@free.fr>
2021-06-11 08:34:44 +0200

    add trap_entry to nuclei freertos for compatibility reasons

--- freeRTOS_port.cmake
+++ freeRTOS_port.cmake
@@ -1,8 +1,8 @@
 SET(FOS ${ARDUINO_GD32_FREERTOS}/FreeRTOS)
-IF(0)
+IF(1)
     # Nuclei version
     SET(LN_FREERTOS_PORT ${ARDUINO_GD32_FREERTOS}/freeRTOS_extension/N200_nuclei )
-    SET(N200  ${LN_FREERTOS_PORT}/port.c ${LN_FREERTOS_PORT}/portasm.S   ${LN_FREERTOS_PORT}/port_interrupt.c)
+    SET(N200  ${LN_FREERTOS_PORT}/port.c ${LN_FREERTOS_PORT}/portasm.S   ${LN_FREERTOS_PORT}/port_interrupt.c ${LN_FREERTOS_PORT}/trap.S)
 ELSE()
     # QQ version
     SET(LN_FREERTOS_PORT ${ARDUINO_GD32_FREERTOS}/freeRTOS_extension/N200 )
