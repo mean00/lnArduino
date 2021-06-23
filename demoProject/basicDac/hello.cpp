ee9b0291b53105e0de2cddc1ecb17431df494cbc
mean <fixounet@free.fr>
2021-06-23 07:24:48 +0200

    signal generator

--- demoProject/basicDac/hello.cpp
+++ demoProject/basicDac/hello.cpp
@@ -1,6 +1,7 @@
 #include "lnArduino.h"
 #include "lnTimer.h"
 #include "lnDAC.h"
+#include "math.h"
 #define LED PA2
 #define PWM PB9
 #define TIMER 3
@@ -31,10 +32,32 @@ void loop()
     
       lnDAC *dac=new lnDAC(0); 
       
-      dac->startDmaMode();
+      int signalFrequency=30*1000;
+      
+      dac->startDmaMode(signalFrequency*100);
+      int actualFq=dac->getDmaFrequency();
+      
+    float pointFloat=(float)(actualFq+signalFrequency/2)/(float)signalFrequency;
+    int nbPoints=floor(pointFloat);
+    Logger("In Fq=%d outFq=%d, # points=%d pointsF=%f\n",signalFrequency,actualFq,nbPoints,pointFloat);
+    
+    uint16_t xsin[nbPoints];
+    for(int i=0;i<nbPoints;i++)
+    {
+        float angle=2.*M_PI;
+        angle/=(float)nbPoints;
+        angle*=(float)i;        
+        xsin[i]=2048.+2047.*sin(angle);
+    }
       
       
-      dac->doDma(100000);
+    dac->dmaWrite(nbPoints,xsin,true);
+    while(1)
+    {
+        xDelay(10);
+    }
+      
+      dac->doDma(30*1000);
       xAssert(0);
       dac->simpleMode();
       dac->enable();
