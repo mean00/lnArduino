845be701cac8946cf81029e598c1e8a9051c9fd9
mean <fixounet@free.fr>
2021-07-08 07:45:27 +0200

    2 steps init for arm (SWD)

--- src/lnRunTime_arm.cpp
+++ src/lnRunTime_arm.cpp
@@ -1,12 +1,20 @@
 #include "lnArduino.h"
 
+
+extern void lnExtiSWDOnly();
+/**
+ * 
+ */
 void lnRunTimeInit()
 {
+}
+/**
+ * 
+ * @return 
+ */
+void lnRunTimeInitPostPeripherals()
+{
     // Init
-    
-    // 
-    
-    
-
+    lnExtiSWDOnly();
 }
 // EOF
