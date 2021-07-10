a6f18877ebfe9245c410606eaef98033ec869966
mean <fixounet@free.fr>
2021-07-10 09:29:42 +0200

    arm scb

--- private_include/lnSCB_arm_priv.h
+++ private_include/lnSCB_arm_priv.h
@@ -9,6 +9,10 @@ struct LN_SCB_Registersx
     uint32_t CPUID;
     uint32_t ICSR;
     uint32_t VTOR;
+    uint32_t AIRCR; 
+    uint32_t SCR; 
+    uint32_t CR; 
+    uint8_t  SHP[12];  // system exception priority
 };
 
 typedef volatile LN_SCB_Registersx LN_SCB_Registers;
