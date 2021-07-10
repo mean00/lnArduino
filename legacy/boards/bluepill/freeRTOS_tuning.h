f5f74da768e0d9010723f99a3e3e4d0470a3c944
mean <fixounet@free.fr>
2021-07-10 20:14:27 +0200

    set freeRTOS for gd32/stm32

--- legacy/boards/bluepill/freeRTOS_tuning.h
+++ legacy/boards/bluepill/freeRTOS_tuning.h
@@ -3,7 +3,7 @@
 #define configPRIO_BITS                                 (4UL)
 #define configLIBRARY_LOWEST_INTERRUPT_PRIORITY         0xf
 #define configKERNEL_INTERRUPT_PRIORITY                 ( configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
-#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY	0xf
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY	5
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY            ( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
 
 #define configCPU_CLOCK_HZ			( ( uint32_t ) SystemCoreClock) 
