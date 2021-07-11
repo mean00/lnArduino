a40d415217c90033acf7651b586822a2521e0d94
mean <fixounet@free.fr>
2021-07-11 16:56:38 +0200

    split systemTimer between arm and riscv

--- src/lnRCU_systemClock_riscv.cpp
+++ src/lnRCU_systemClock_riscv.cpp
@@ -1,4 +1,7 @@
-
+/*
+ *  (C) 2021 MEAN00 fixounet@free.fr
+ *  See license file
+ */
 #include "lnArduino.h"
 #include "lnRCU.h"
 #include "lnRCU_priv.h"
---
+++ src/lnSystemTimer_arm.cpp
@@ -0,0 +1,8 @@
+#include "lnArduino.h"
+
+
+extern "C" uint64_t lnGetUs()
+{  
+#warning FIXME!
+return 0;
+}
