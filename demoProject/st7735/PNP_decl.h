e512fff06ab9c52825a97eadf9dcd93b1458f4bf
mean <fixounet@free.fr>
2021-07-13 07:45:40 +0200

    external ST7735 demo for Arm board

---
+++ demoProject/st7735/PNP_decl.h
@@ -0,0 +1,3 @@
+#define PNP_width 48
+#define PNP_height 48
+extern const unsigned char PNP[];
---
+++ demoProject/st7735/hello.cpp
@@ -0,0 +1,110 @@
+//
+// This is a demo using st7735 1.8 inch 128x160 screen
+//
+//  Color          LCD Pin    MCU PIn
+//  Grey           CS         PA4
+//  Purple         Reset      PA0
+//  Blue           A0/DC      PA12
+//  Green          SDA        PA7
+//  Brown          SCK        PA5
+//
+
+#include "lnArduino.h"
+#include "lnSPI.h"
+#include "gd32ST7735.h"
+#include "FreeSans7pt7b.h"
+#include "PNP_compressed.h"
+#include "PNP_decl.h"
+
+#define SCREEN_WIDTH  128
+#define SCREEN_HEIGHT 160
+
+#define splash_width 128
+#define splash_height 96
+
+
+#define LED LN_SYSTEM_LED
+
+// MOSI is PA7
+// CLK  is PA5
+// MISO is PA6
+
+#define PINDC  PA12
+#define PINCS  PA4
+#define PINRST PA0
+// 
+/**
+ * 
+ */
+void setup()
+{
+#define WAIT 15
+    pinMode(LED,OUTPUT);
+    digitalWrite(LED,HIGH);
+    
+    uint32_t before=millis();
+    delayMicroseconds(WAIT*1000);
+    uint32_t after=millis();
+    Logger("Should be ~ %d : %d, %d delta= %d  \n",WAIT,before,after,(int)(after-before));
+    
+    int usBefore=micros();
+    xDelay(WAIT);
+    int usAfter=micros();
+    Logger("Should be %d x 1000 : %d->%d,delta=%d  \n",WAIT,usBefore,usAfter,(int)(usAfter-usBefore));
+    
+}
+/**
+ */
+void loop()
+{
+    Logger("Entering main app...\n");
+   
+    hwlnSPIClass *spi=new hwlnSPIClass(0,-1);
+    spi->begin();
+    lnSPISettings transaction(30*1000*1000, SPI_MSBFIRST, SPI_MODE0,-1);
+    spi->beginTransaction(transaction);
+    // Reset LCD
+    pinMode(PINRST,OUTPUT);
+    digitalWrite(PINRST,HIGH);
+    xDelay(50);
+    digitalWrite(PINRST,LOW);
+    xDelay(50);
+    digitalWrite(PINRST,HIGH);
+    
+    gd32ST7735 *lcd=new gd32ST7735(SCREEN_WIDTH,SCREEN_HEIGHT,spi,PINDC,PINCS);
+    lcd->init();
+    lcd->setRotation(1);
+    lcd->fillScreen(0);
+    
+    lcd->drawRLEBitmap(PNP_width,PNP_height,0,0,0x1f<<6,0x1f<<11,PNP);
+    
+    // init fonts
+    lcd->setFontFamily(&FreeSans7pt7b,&FreeSans7pt7b,&FreeSans7pt7b);
+    lcd->setFontSize(st7735::SmallFont);    
+    
+    
+    int r=0;
+    int roundup;
+    bool onoff;
+    while(1)
+    {
+        roundup++;
+        vTaskDelay(1200);
+        digitalToggle(LED);
+        onoff=!onoff;
+        if(onoff)
+        {
+            lcd->square(0x1f,10,20,40,20);
+            lcd->print(20,20,"This is  a test");
+        }else
+        {
+            lcd->square(0xffff,10,20,40,20);
+        }
+        r++;
+        r&=15;
+        lcd->setRotation(r>>2);
+        Logger("Blue\n");
+    }
+    
+}
+// EOF
