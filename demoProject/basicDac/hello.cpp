f857b158ea7ac814a7cec68c00622ad50b93375e
mean <fixounet@free.fr>
2021-07-26 08:21:23 +0200

    fix dac

--- demoProject/basicDac/hello.cpp
+++ demoProject/basicDac/hello.cpp
@@ -2,15 +2,14 @@
 #include "lnTimer.h"
 #include "lnDAC.h"
 #include "math.h"
-#define LED PA2
+
 #define PWM PB9
 #define TIMER 3
 #define CHANNEL 3
 
 void setup()
 {
-    pinMode(LED,OUTPUT);
-    
+    pinMode(LN_SYSTEM_LED,OUTPUT);    
 }
 /**
  * 
@@ -21,27 +20,25 @@ void setup()
 void loop()
 {
     bool onoff=true;
-    digitalWrite(LED,true);
+    digitalWrite(LN_SYSTEM_LED,true);
     int roundup=0;
 
-//#define mode   LN_CTL_INPUT_ANALOG
-#define mode  lnDAC_MODE
     
-      pinMode(PA4,mode);
-      pinMode(PA5,mode);
+    lnPinMode(PA4,lnDAC_MODE);
+    lnPinMode(PA5,lnDAC_MODE);
     
-      lnDAC *dac=new lnDAC(0); 
+    lnDAC *dac=new lnDAC(0); 
       
-      int signalFrequency=30*1000;
+    int signalFrequency=30*1000;
       
-      dac->startDmaMode(signalFrequency*100);
-      int actualFq=dac->getDmaFrequency();
+    dac->startDmaMode(signalFrequency*100);
+    int actualFq=dac->getDmaFrequency();
       
     float pointFloat=(float)(actualFq+signalFrequency/2)/(float)signalFrequency;
     int nbPoints=floor(pointFloat);
     Logger("In Fq=%d outFq=%d, # points=%d pointsF=%f\n",signalFrequency,actualFq,nbPoints,pointFloat);
-    
-    uint16_t xsin[nbPoints];
+
+    uint16_t *xsin=new uint16_t[nbPoints];
     for(int i=0;i<nbPoints;i++)
     {
         float angle=2.*M_PI;
@@ -50,22 +47,23 @@ void loop()
         xsin[i]=2048.+2047.*sin(angle);
     }
       
-      
+    
     dac->dmaWrite(nbPoints,xsin,true);
+       
     while(1)
     {
         xDelay(10);
     }
-      
-      xAssert(0);
-      dac->simpleMode();
-      dac->enable();
+#if 0         
+    xAssert(0);
+    dac->simpleMode();
+    dac->enable();
 
-      while(1)
-      {
-          dac->setValue(val);
-          val++;
-          val&=(1<<13)-1;
-      }
-    
+    while(1)
+    {
+        dac->setValue(val);
+        val++;
+        val&=(1<<13)-1;
+    }
+#endif    
 }
