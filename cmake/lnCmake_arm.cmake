248c5967990881894bd0a71eb52e1fc240dcb053
mean <fixounet@free.fr>
2021-08-06 19:55:40 +0200

    make linker region size configurable

--- cmake/lnCmake_arm.cmake
+++ cmake/lnCmake_arm.cmake
@@ -1,10 +1,24 @@
 
 MACRO(GENERATE_GD32_FIRMWARE target)
+
+    # Set default value
+    IF(NOT LN_MCU_RAM_SIZE)
+        SET(LN_MCU_RAM_SIZE 20)
+    ENDIF(NOT LN_MCU_RAM_SIZE)
+    IF(NOT LN_MCU_FLASH_SIZE)
+        SET(LN_MCU_FLASH_SIZE 64)
+    ENDIF(NOT LN_MCU_FLASH_SIZE)
+    IF(NOT LN_MCU_EEPROM_SIZE)
+        SET(LN_MCU_EEPROM_SIZE 4)
+    ENDIF(NOT LN_MCU_EEPROM_SIZE)
+
+    configure_file( "${AF_FOLDER}/legacy/boards/bluepill/ld.lds.in" "${CMAKE_BINARY_DIR}/linker_script.ld" @ONLY)
     ADD_EXECUTABLE(${target} ${ARGN}  ${AF_FOLDER}/arm/start.S   ${AF_FOLDER}/arm/start.cpp 
                                                                  ${AF_FOLDER}/arm/vector_table.S) 
     TARGET_LINK_LIBRARIES(${target} ${USED_LIBS} ) # duplicates are NOT a mistake !
-    TARGET_LINK_LIBRARIES(${target} embeddedPrintf gd32_overlay gd32Arduino   FreeRTOS  gd32_lowlevel c  c gcc ) # dupicates are NOT a mistake !
-    TARGET_LINK_OPTIONS(${target}  PRIVATE "-T${AF_FOLDER}/legacy/boards/${GD32_BOARD}/ld.lds" )
+    # duplicates are NOT a mistake !
+    TARGET_LINK_LIBRARIES(${target} embeddedPrintf gd32_overlay gd32Arduino   FreeRTOS  gd32_lowlevel c  c gcc ) 
+    TARGET_LINK_OPTIONS(${target}  PRIVATE "-T${CMAKE_BINARY_DIR}/linker_script.ld")
 ENDMACRO(GENERATE_GD32_FIRMWARE target)
 
 MACRO(USE_LIBRARY lib)
