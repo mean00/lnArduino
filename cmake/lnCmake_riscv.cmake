045205b6559772deb77c148a7a476035cc08269c
mean <fixounet@free.fr>
2021-08-07 08:08:26 +0200

    customisable flash/ram size for riscv also

--- cmake/lnCmake_riscv.cmake
+++ cmake/lnCmake_riscv.cmake
@@ -1,9 +1,22 @@
 
 MACRO(GENERATE_GD32_FIRMWARE target)
+
+  IF(NOT LN_MCU_RAM_SIZE)
+        SET(LN_MCU_RAM_SIZE 32)
+    ENDIF(NOT LN_MCU_RAM_SIZE)
+    IF(NOT LN_MCU_FLASH_SIZE)
+        SET(LN_MCU_FLASH_SIZE 128)
+    ENDIF(NOT LN_MCU_FLASH_SIZE)
+    IF(NOT LN_MCU_EEPROM_SIZE)
+        SET(LN_MCU_EEPROM_SIZE 4)
+    ENDIF(NOT LN_MCU_EEPROM_SIZE)
+
+   configure_file( "${AF_FOLDER}/legacy/boards/sipeed-longan-nano/ld.lds.in" "${CMAKE_BINARY_DIR}/linker_script.lds" @ONLY)
+
     ADD_EXECUTABLE(${target}  ${AF_FOLDER}/riscv/start.S ${ARGN})
     TARGET_LINK_LIBRARIES(${target} ${USED_LIBS} ) # duplicates are NOT a mistake !
     TARGET_LINK_LIBRARIES(${target} embeddedPrintf gd32_overlay gd32Arduino   FreeRTOS  gd32_lowlevel c  c gcc ) # dupicates are NOT a mistake !
-    TARGET_LINK_OPTIONS(${target}  PRIVATE "-T${AF_FOLDER}/legacy/boards/${GD32_BOARD}/ld.lds" )
+    TARGET_LINK_OPTIONS(${target}  PRIVATE "-T${CMAKE_BINARY_DIR}/linker_script.lds" )
 ENDMACRO(GENERATE_GD32_FIRMWARE target)
 
 MACRO(USE_LIBRARY lib)
