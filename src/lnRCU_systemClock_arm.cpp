5f898bec18e4ae6f12779fe461ae5a0d1b10727a
mean <fixounet@free.fr>
2021-08-15 14:45:40 +0200

    use internal oscillator and get to 72M still

--- src/lnRCU_systemClock_arm.cpp
+++ src/lnRCU_systemClock_arm.cpp
@@ -76,61 +76,117 @@ static void waitCfg0Bit(int mask)
 
 static const int Multipliers[]={
     0,0,0,1,     // 0 1 2 3
-    2,3,4,5,6,7, // 4 5 6 7 8 9
-    8,9,10,11,12,13 // 10 11 12 13 14 15
+    2,3,4,5,     // 4 5 6 7 
+    6,7,8,9,     // 8 9 10 11
+    // the ones below dont work on stm32 !
+    10,11,12,12, // 12 13 14 15
+    14,16,17,18 // 16 17 18 19            
 };
-
-static void setPll(int multiplier, int predivider, bool external)
+/**
+ * 
+ * @param multiplier
+ * @param predivider
+ * @param external
+ */
+void setPll(int inputClock, int multiplier, int predivider, bool external)
 {
     volatile uint32_t *control=&(arcu->CTL);
     volatile uint32_t *cfg0=&(arcu->CFG0);
     volatile uint32_t *cfg1=&(arcu->CFG1);
     
     uint32_t c0=*cfg0;
-    
+    *cfg1=0;
     int pllMultiplier=Multipliers[multiplier];
     
-    SystemCoreClock=(CLOCK_XTAL_VALUE*multiplier*1000000)/predivider;
+    SystemCoreClock=(inputClock*multiplier*1000000)/predivider;
     _rcuClockApb1=SystemCoreClock/2;
     _rcuClockApb2=SystemCoreClock;
     
     // Set PLL multiplier    
     c0&=~((0xf)<<18);    
-    c0|=((pllMultiplier)&0x0f)<<18; // PLL Multiplier, ignore MSB        
+    c0|=((pllMultiplier)&0x0f)<<18; // PLL Multiplier, ignore MSB      
+    if(pllMultiplier&0x10)
+        c0|= 1<<27;
+    else
+        c0&=~(1<<27);
     if(external)
         c0 |=LN_RCU_CFG0_PLLSEL;
     else
         c0 &=~LN_RCU_CFG0_PLLSEL;
     *cfg0=c0;
+    
+    // start PLL...
+    *control|=LN_RCU_CTL_PLLEN;
+    waitControlBit(LN_RCU_CTL_PLLSTB); // Wait Xtal stable
 }
 
 //
+#define LN_CLOCK_IRC8  0
+#define LN_CLOCK_XTAL  16
+#define LN_CLOCK_PLL   24
+static void enableDisableClock(int clock, bool enabled)
+{
+    int setBit=1<<clock;
+    int statusBit=1<<(clock+1);
+    if(enabled)
+    {
+        arcu->CTL|=setBit;
+        waitControlBit(statusBit); 
+    }else
+    {
+        arcu->CTL&=~setBit;
+    }
+    
+}
+/**
+ * 
+ */
 void lnInitSystemClock()
 {
     lnCpuID::identify();
     
+    //________________________
+    // Reset everything...
+    //________________________
+    // Enable IRC8 just in case
+    enableDisableClock(LN_CLOCK_IRC8,true);
+    // Switch SYS clock to IRC8
+    uint32_t sysClock=arcu->CFG0;
+    sysClock&=LN_RCU_CFG0_SYSCLOCK_MASK; //  0 is IRC8
+    arcu->CFG0=sysClock;
+    while(1)
+    {
+        if(!(arcu->CFG0&(LN_RCU_CFG0_SYSCLOCK_MASK<<2))) break;
+    }
+    // Ok now we can disable PLL and Xtal
+    enableDisableClock(LN_CLOCK_XTAL,false);
+    enableDisableClock(LN_CLOCK_PLL,false);
+    
+    //________________________
+    //
+    //________________________
+    
     volatile uint32_t *control=&(arcu->CTL);
     volatile uint32_t *cfg0=&(arcu->CFG0);
+    
+    int inputClock;
+    bool useExternalClock;
+    
 #ifndef LN_USE_INTERNAL_CLOCK       
     {
         // start crystal...
         *control|=LN_RCU_CTL_HXTALEN;
         waitControlBit(LN_RCU_CTL_HXTASTB); // Wait Xtal stable
-    }
-     {
-        setPll(CLOCK_TARGET_SYSCLOCK/CLOCK_XTAL_VALUE,CLOCK_TARGET_PREDIV,true); // 8*9/1=72 Mhz        
-    }
-    {
-        // start PLL...
-        *control|=LN_RCU_CTL_PLLEN;
-        waitControlBit(LN_RCU_CTL_PLLSTB); // Wait Xtal stable
-    }
+        inputClock=CLOCK_XTAL_VALUE;
+        useExternalClock=true;
 #else
-    setPll(CLOCK_TARGET_SYSCLOCK/CLOCK_XTAL_VALUE,CLOCK_TARGET_PREDIV,false); // 8*9/1=72 Mhz        
-    *control|=LN_RCU_CTL_PLLEN;
-    waitControlBit(LN_RCU_CTL_PLLSTB); // Wait Xtal stable
-#endif    
-   
+        inputClock=8/2; // HSI is divided by 2
+        useExternalClock=false;
+#endif        
+    
+    int multiplier=CLOCK_TARGET_SYSCLOCK/inputClock;
+    setPll(inputClock,multiplier,CLOCK_TARGET_PREDIV,useExternalClock); // 8*9/1=72 Mhz        
+    
     // Setup AHB...
     // AHB is Xtal:1, divider value=0    
     // APB2=AHB/1
