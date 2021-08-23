e31e6c071c99883818f9fc3f1ab20301ca9f4b82
mean <fixounet@free.fr>
2021-08-23 14:45:51 +0200

    add TaskSuspend so that gdb can find stuff

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -78,7 +78,7 @@ to exclude the API function. */
 #define INCLUDE_uxTaskPriorityGet		0
 #define INCLUDE_vTaskDelete			1
 #define INCLUDE_vTaskCleanUpResources           0
-#define INCLUDE_vTaskSuspend			0
+#define INCLUDE_vTaskSuspend			1
 #define INCLUDE_vTaskDelayUntil			1
 #define INCLUDE_vTaskDelay			1
 
