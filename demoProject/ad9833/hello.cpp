45a7774e2422384abba1e1a32c2f669b7df78e2a
mean <fixounet@free.fr>
2021-09-07 08:18:00 +0200

    simple manual driving

--- demoProject/ad9833/hello.cpp
+++ demoProject/ad9833/hello.cpp
@@ -4,6 +4,7 @@
 #include "lnArduino.h"
 #include "lnSPI.h"
 #include "simplerAD9833.h"
+#include "libraries/simplerAD9833/simplerAD9833.h"
 
 
 #define AD9833_CS  PB11
@@ -12,6 +13,8 @@
 simplerAD9833 *ad;
 hwlnSPIClass *spi;
 
+
+void spiLoop();
 /**
  * 
  */
@@ -22,11 +25,66 @@ void setup()
     Logger("Starting AD9833...");
     spi=new hwlnSPIClass(AD9833_SPI);
     spi->begin();
-    spi->setSpeed(100*1000); // dont care much about the speed    
-    ad=new simplerAD9833(spi,AD9833_CS);
+    spi->setSpeed(1000); // dont care much about the speed    
+    spi->setBitOrder(SPI_MSBFIRST);
+    spi->setDataMode(SPI_MODE2);
     
+    pinMode(AD9833_CS,OUTPUT);
+    digitalWrite(AD9833_CS,true);
+    spiLoop();
+    ad=new simplerAD9833(spi,AD9833_CS);    
 }
 
+void wr(int x)
+{
+        digitalWrite(AD9833_CS,false);
+        xDelay(1);
+        spi->write(x>>8);
+        spi->write(x&0xff);
+        xDelay(1);
+        digitalWrite(AD9833_CS,true);
+        xDelay(1);
+}
+#include "simplerAD9833_internal.h"
+void spiLoop()
+{
+        // Reset
+        wr(LN_AD9833_RESET);
+        xDelay(1);
+        //
+        wr(0x00);
+#if 0 // this works ~ 5 Mhz        
+    while(1)
+    {
+        wr(LN_AD9833_OPBITEN);
+        xDelay(2000);
+        wr(LN_AD9833_OPBITEN+LN_AD9833_DIV2);
+        xDelay(2000);
+        wr(0);
+        xDelay(2000);
+       // wr(LN_AD9833_HLB); // Wrote
+       // wr(LN_AD9833_CTRL(1)+0xf00); // Wrote
+        
+        //xDelay(2000);
+        
+    }
+#endif   
+    // ~ 100 hz
+#define CMD LN_AD9833_OPBITEN*0
+    while(1)
+    {
+        //wr(CMD);       
+        //xDelay(1);
+        wr(LN_AD9833_B28+CMD); // Wrote
+        wr(LN_AD9833_FREQ+0xf00); // Wrote
+        wr(LN_AD9833_FREQ+0x000); // Wrote
+        
+        xDelay(2000);
+        
+    }
+        
+        
+}
 // M3 @ 112 Mhz Multiply =44, refresh=6 ms
 // M4 @ 112 Mhz Multiply =9, refresh=6 ms
 
@@ -38,10 +96,14 @@ void loop()
     Logger("Entering main app...\n");
     int onoff=0;
     
-    ad->setWaveForm(simplerAD9833::Square);
-    ad->setFrequency(1000);
+    ad->setWaveForm(simplerAD9833::Triangle);
+    //ad->setFrequency(1000);
     ad->enable();
+    //ad->setWaveForm()
     while(1)
+    {
+        xDelay(1000);
+    }
     {
      ad->disable();
      ad->setFrequency(1000);
