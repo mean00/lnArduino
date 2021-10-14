b95dd413a9a38cee6f1256a1c6220c106fc9a56c
mean <fixounet@free.fr>
2021-10-14 07:37:17 +0200

    add simple example

---
+++ demoProject/flatBlink/hello.cpp
@@ -0,0 +1,32 @@
+#include "lnArduino.h"
+
+
+#ifdef __arm__ 
+#define LED PC13
+#else
+#define LED PA2
+#endif
+
+void setup()
+{
+    pinMode(LED,OUTPUT);
+    
+}
+/**
+ * 
+ */
+int roundup=0;
+void loop()
+{
+    bool onoff=true;
+    digitalWrite(LED,true);
+    
+    while(1)
+    {
+        roundup++;
+        xDelay(500);
+        digitalToggle(LED);
+        onoff=!onoff;
+        Logger("*Hey*\n");
+    }
+}
