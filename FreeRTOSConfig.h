caf77f657a539f52a14b2903e6196b5f3a5c4fee
mean <fixounet@free.fr>
2021-10-24 12:04:46 +0200

    enable taskNotification

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -59,7 +59,9 @@ extern void do_assert(const char *a);
 #define configUSE_TRACE_FACILITY                0
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
-
+//
+#define configUSE_TASK_NOTIFICATIONS            1
+#define configTASK_NOTIFICATION_ARRAY_ENTRIES   5
 
 // If we use timers
 #define configTIMER_TASK_PRIORITY               5
