7c1f7278ae61deb566b9f50fc73f1324acc65f0a
mean <fixounet@free.fr>
2021-11-19 07:02:30 +0100

    skeleton for test/delayTimer

---
+++ include/lnBasicTimer.h
@@ -0,0 +1,56 @@
+#pragma once
+
+
+typedef void (lnBasicTimerIrq)(void *cookie);
+/**
+ * \brief Careful all the channels of a given timer share the same frequency!
+ * Max frequency is around 100 kHz in PWM mode !
+ * 
+ * 
+ * In all mode , after setup the channel is disabled
+ * You have to explicitely call timer->enable()
+ * 
+ * @param timer
+ * @param channel
+ */
+class lnBasicTimer
+{
+public:
+             lnBasicTimer(int timer);
+    virtual ~lnBasicTimer();
+        // PWM
+        void setTimerFrequency(int fqInHz); // This program the timer fq with a tick of 1024, i.e. internal fq is 1024* the given fq
+        void setTimerFrequency(int prediv, int reload); 
+        int  getTimerFrequency();
+        void disable();
+        void enable();
+        void enableInterrupt();
+        void disableInterrupt();
+        void setInterrupt(lnBasicTimerIrq *handler, void *cookie);
+protected:
+        
+        int _timer;
+};
+/**
+ * 
+ * @param timer
+ * @return 
+ */
+class lnBasicDelayTimer : lnBasicTimer
+{
+public:
+             lnBasicDelayTimer(int timer);
+    virtual ~lnBasicDelayTimer();
+        void arm(int delayUs);
+        void setInterrupt(lnBasicTimerIrq *handler, void *cookie);
+        //--
+        static void interruptHandler(int timer);
+protected:
+        void irq();
+        void enableInterrupt();
+        void disableInterrupt();
+        
+        lnBasicTimerIrq *_handler;
+        void             *_cookie;
+        
+};
