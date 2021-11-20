799076c9511ee2b2148539bd9efda8e67a1282c8
mean <fixounet@free.fr>
2021-11-20 08:28:25 +0100

    delayTimer test case

--- demoProject/timerIrq/hello.cpp
+++ demoProject/timerIrq/hello.cpp
@@ -28,17 +28,21 @@ void loop()
     timer=new lnBasicDelayTimer(0);
     sem=new xBinarySemaphore;
     timer->setInterrupt(handler,sem);
-   
+    Logger("Go!\n"); 
     while(1)
     {
-        uint32_t now=lnGetUs();
-        timer->arm(500);
-        sem->take();
-        int delta=lnGetUs()-now;
-        delta-=500;
-        if(delta<0) delta=-delta;
-        Logger("Error = %d\n",delta);
-        xDelay(300);
+        for(int del=500;del<1000;del+=100)
+            for(int i=0;i<5;i++)
+            {
+                uint32_t now=lnGetUs();
+                timer->arm(del);
+                sem->take();
+                int delta=lnGetUs()-now;
+                delta-=del;
+                if(delta<0) delta=-delta;
+                Logger("%d Error = %d (now=%d)\n",del,delta,now&0xffff);
+                xDelay(300);
+            }
     }
     
 }
