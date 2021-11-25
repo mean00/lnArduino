edc81e41760104b05eb157a39b64fef97d2fe250
mean <fixounet@free.fr>
2021-11-25 07:56:55 +0100

    tweak longan heap size

--- legacy/boards/sipeed-longan-nano/freeRTOS_tuning.h
+++ legacy/boards/sipeed-longan-nano/freeRTOS_tuning.h
@@ -12,5 +12,5 @@
 #define configRTC_CLOCK_HZ                      ((uint32_t)TIMER_FREQ)
 
 #define configMINIMAL_STACK_SIZE                ( ( unsigned short ) 128 )
-#define configTOTAL_HEAP_SIZE                   ( ( size_t ) (  26 * 1024 ) ) 
+#define configTOTAL_HEAP_SIZE                   ( ( size_t ) (  25 * 1024 ) ) 
 
