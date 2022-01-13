9f5df8a674d08dd3a21122e188a02fd8cdd706e3
mean <fixounet@free.fr>
2022-01-13 08:49:50 +0100

    rust stuff, arm only

--- rust/rnArduino/lib/bindgen.sh
+++ rust/rnArduino/lib/bindgen.sh
@@ -1,1 +1,7 @@
-bindgen     --whitelist-function  "^[lL][nN].*" --no-layout-tests      lnAll.h  -o rnArduino.rs -- -x c++   -DLN_ARCH=LN_ARCH_ARM -I../include -I../arduinoLayer/include/ -I../FreeRTOS/include/ -I.. -I../legacy/boards/bluepill/ -I../FreeRTOS/portable/GCC/ARM_CM3/
+set -x
+export LN=$PWD/../../..
+bindgen   --use-core --ctypes-prefix cty --whitelist-function  "^[lL][nN].*" --no-layout-tests      lnAll.h  -o rnArduino.rs.tmp -- -x c++   -DLN_ARCH=LN_ARCH_ARM \
+    -I$LN -I$LN/include  -I$LN/arduinoLayer/include/ -I$LN/FreeRTOS/include/  -I$LN/legacy/boards/bluepill/ -I$LN/FreeRTOS/portable/GCC/ARM_CM3/
+
+echo " #![no_std]" > rnArduino.rs
+cat rnArduino.rs.tmp >> rnArduino.rs
