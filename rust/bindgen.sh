6fcfdec6a12949aa26dc31ab44273eec26298226
mean <fixounet@free.fr>
2022-01-13 07:57:27 +0100

    add rust

---
+++ rust/bindgen.sh
@@ -0,0 +1,1 @@
+bindgen     --whitelist-function  "^[lL][nN].*" --no-layout-tests      lnAll.h  -o lnSPI.rs -- -x c++   -DLN_ARCH=LN_ARCH_ARM -I../include -I../arduinoLayer/include/ -I../FreeRTOS/include/ -I.. -I../legacy/boards/bluepill/ -I../FreeRTOS/portable/GCC/ARM_CM3/
