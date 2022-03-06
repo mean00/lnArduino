eaa265742803812a25a502078ea6ac15baf44103
mean <fixounet@free.fr>
2022-03-06 17:52:48 +0100

    export Logger as C linkage

--- src/lnDebug.cpp
+++ src/lnDebug.cpp
@@ -13,48 +13,47 @@ extern lnSerial *serial0=NULL;
 extern "C" void Logger_C(const char *fmt,...)
 {
   static char buffer[128];
-  
+
   va_list va;
   va_start(va,fmt);
   vsnprintf(buffer,127,fmt,va);
-  
+
   buffer[127]=0;
-#ifdef LOGGER_USE_DMA    
-  serial0->dmaTransmit(strlen(buffer),(uint8_t *)buffer);    
+#ifdef LOGGER_USE_DMA
+  serial0->dmaTransmit(strlen(buffer),(uint8_t *)buffer);
 #else
   serial0->transmit(strlen(buffer),(uint8_t *)buffer);
 #endif
-  va_end(va);    
+  va_end(va);
 }
 
 
 /**
- * 
+ *
  * @param fmt
  */
 void Logger(const char *fmt...)
 {
     static char buffer[128];
-    
+
     va_list va;
     va_start(va,fmt);
     vsnprintf(buffer,127,fmt,va);
-    
+
     buffer[127]=0;
-#ifdef LOGGER_USE_DMA    
-    serial0->dmaTransmit(strlen(buffer),(uint8_t *)buffer);    
+#ifdef LOGGER_USE_DMA
+    serial0->dmaTransmit(strlen(buffer),(uint8_t *)buffer);
 #else
     serial0->transmit(strlen(buffer),(uint8_t *)buffer);
 #endif
-    va_end(va);    
+    va_end(va);
 }
 /**
- * 
+ *
  */
 void LoggerInit()
 {
     serial0=new lnSerial(0);
-    serial0->init();  
-    serial0->setSpeed(115200);    
+    serial0->init();
+    serial0->setSpeed(115200);
 }
-
