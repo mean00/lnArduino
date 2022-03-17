e0991a8d20f1b5e601fb95c2f2afe9d8317e0119
mean <fixounet@free.fr>
2022-03-17 18:57:46 +0100

    prevent the linker from removing the freeRTOSDebug block

--- include/lnFreeRTOSDebug.h
+++ include/lnFreeRTOSDebug.h
@@ -4,6 +4,7 @@
 
 struct lnFreeRTOSDebug
 {
+  int MAGIC;
   int NB_OF_PRIORITIES;
   int MPU_ENABLED;
   int MAX_TASK_NAME_LEN;
--- src/lnFreeRTOSDebug.cpp
+++ src/lnFreeRTOSDebug.cpp
@@ -4,8 +4,21 @@
  * */
 #include "lnFreeRTOSDebug.h"
 
-extern const lnFreeRTOSDebug freeRTOSDebug LN_USED={
+extern const uint32_t *lnGetFreeRTOSDebug() LN_USED;
+
+static const lnFreeRTOSDebug freeRTOSDebug LN_USED={
+  0x1FEEBAEUL,
   configMAX_PRIORITIES,//int NB_OF_PRIORITIES;
   0, //int MPU_ENABLED;
   configMAX_TASK_NAME_LEN, // MAX_TASK_NAME_LEN
 };
+/*
+  dummy function to prevent the linker from removing it
+*/
+const uint32_t *lnGetFreeRTOSDebug()
+{
+    return (uint32_t *)&freeRTOSDebug;
+}
+
+
+// EOF
