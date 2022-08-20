391bdf10f806fb4968c286b5e669585beb2a4d61
mean <fixounet@free.fr>
2022-08-20 10:19:36 +0200

    cleanup bindgen, on the fly patch lnArduino uint32_t to be unsigned long so that both rust and gcc agree on what is a u32 on armv7

--- rust/rnArduino/src/bindgen.sh
+++ rust/rnArduino/src/bindgen.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+export PLATFORM_TOOLCHAIN_PATH=/home/fx/Arduino_stm32/arm-gcc-2021q4/bin
+export  PATH=$PLATFORM_TOOLCHAIN_PATH:$PATH
 export LN=` realpath $PWD/../../..`
+export out=rnarduino.rs
 echo "Processing $LN"
 \rm -f rnArduino.rs.tmp
-bindgen   --use-core --ctypes-prefix cty  --no-layout-tests      lnAll.h  -o rnArduino.rs.tmp -- -x c++   -DLN_ARCH=LN_ARCH_ARM \
-    -I$LN -I$LN/include/  -I$LN/arduinoLayer/include/ -I$LN/FreeRTOS/include/  -I$LN/legacy/boards/bluepill/ -I$LN/FreeRTOS/portable/GCC/ARM_CM3/
-
-cat header.rs.in > lib.rs
-cat rnArduino.rs.tmp >> lib.rs
-cat tail.rs.in >> lib.rs
+bindgen      --use-core --no-doc-comments \
+     --no-layout-tests      lnAll.h \
+     --whitelist-function  "^[lLx][nN].*|deadEnd" \
+    --ctypes-prefix cty \
+    --no-default "uint32_t" \
+   -o rnArduino.rs.tmp -- -x c++   -DLN_ARCH=LN_ARCH_ARM \
+    -I$LN -I$LN/include/  -I$LN/arduinoLayer/include/ -I$LN/FreeRTOS/include/  -I$LN/legacy/boards/bluepill/ -I$LN/FreeRTOS/portable/GCC/ARM_CM3/ \
+    -target "thumbv7m-none-eabi"  \
+    -I${PLATFORM_TOOLCHAIN_PATH}/../arm-none-eabi/include/ -D_UINT32_T_DECLARED -Duint32_t="unsigned long int"
+cat header.rs.in > ${out}
+cat rnArduino.rs.tmp >> ${out}
+cat tail.rs.in >> ${out}
 \rm -f rnArduino.rs.tmp
 
-# --whitelist-function  "^[lLx][nN].*"
