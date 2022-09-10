7eb7e37fe23f4e2828e356b56b133cb622e3d453
mean <fixounet@free.fr>
2022-09-10 08:29:56 +0200

    cleanup rnArduino binding generation script

--- rust/rnArduino/src/bindgen.sh
+++ rust/rnArduino/src/bindgen.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
-# Clang
-export PLATFORM_TOOLCHAIN_PATH=/home/fx/Arduino_stm32/arm-gcc-2021q4/bin
-export  PATH=$PLATFORM_TOOLCHAIN_PATH:$PATH
-
-
 #
-export LN=` realpath $PWD/../../..`
 export out=rnarduino.rs
-echo "Processing $LN"
-\rm -f rnArduino.rs.tmp
 # rustified_enum
-
-bindgen      --use-core --no-doc-comments \
-     --no-layout-tests      lnAll.h \
-    --ctypes-prefix cty \
-    --rustified-enum "ln.*" \
-   -o rnArduino.rs.tmp -- -x c++   -DLN_ARCH=LN_ARCH_ARM \
-    -I$LN -I$LN/include/  -I$LN/arduinoLayer/include/ -I$LN/FreeRTOS/include/  -I$LN/legacy/boards/bluepill/ -I$LN/FreeRTOS/portable/GCC/ARM_CM3/ \
-    -target "thumbv7m-none-eabi"  \
-    -I${PLATFORM_TOOLCHAIN_PATH}/../arm-none-eabi/include/ 
-cat header.rs.in > ${out}
-cat rnArduino.rs.tmp >> ${out}
-cat tail.rs.in >> ${out}
-\rm -f rnArduino.rs.tmp
-#patch -p4 < patched_lnPin_enum.diff 
-     #--whitelist-function  "^[lLx][nN].*|deadEnd|Logger" \
+bash ../../../cmake/rustgen.sh  lnAll.h   ${out}
 
