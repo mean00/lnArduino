09182e11b16d48d1d3ec5a6e5ef3e8192526c329
mean <fixounet@free.fr>
2022-09-18 14:33:35 +0200

    add rnLogger

--- rust/rnArduino/src/rnOsHelper.rs
+++ rust/rnArduino/src/rnOsHelper.rs
@@ -1,11 +1,19 @@
 #![allow(dead_code)]
 
 use crate::rnarduino as rn;
+use cty::c_char;
 
 //--
 pub fn rnDelay(to : i32) -> ()
 {
     unsafe {
         rn::lnDelay(to);
+    }
+}
+
+pub fn  rnLogger( st : &str ) -> ()
+{
+    unsafe {
+        rn::Logger(st.as_ptr() as *const c_char);
     }
 }
