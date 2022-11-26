8e677ef211ce48a9b6f27b18f32bb0934b8c8a0c
mean <fixounet@free.fr>
2022-11-26 08:06:46 +0100

    add single shot

---
+++ demoProject/pwm/single_shot.cpp
@@ -0,0 +1,48 @@
+#include "lnArduino.h"
+#include "lnTimer.h"
+
+
+#define LED LN_SYSTEM_LED
+
+#define PWM_PIN PA6
+
+void setup()
+{
+    pinMode(LED,OUTPUT);    
+}
+int xduration=5;
+/**
+ * 
+ */
+
+void pulse(lnTimer *t, int duration)
+{
+        Logger("duration=%d\n",duration);
+        lnPinMode(PWM_PIN,lnALTERNATE_PP);   
+        t->singleShot(duration); // off by 2%
+        digitalWrite(PWM_PIN,0);
+        lnPinMode(PWM_PIN,lnOUTPUT);
+    
+}
+
+
+void loop()
+{
+    bool onoff=true;
+    digitalWrite(LED,true);
+    int roundup=0;
+    
+    lnPinMode(PWM_PIN,lnPWM);    
+    lnTimer timer(PWM_PIN);
+    
+    Logger("PWM on pin %d\n", PWM_PIN );
+      
+    while(1)
+    {
+    
+        timer.singleShot(1,false);
+        digitalToggle(LED);
+        xDelay(1000);
+    }
+}
+// EOF
