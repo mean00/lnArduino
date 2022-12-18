05a93bc3f1e931a810840cbcdb789d3bf3846cb1
mean <fixounet@free.fr>
2022-12-18 14:26:37 +0100

    add taskCreate

--- rust/rnArduino/src/rnOsHelper.rs
+++ rust/rnArduino/src/rnOsHelper.rs
@@ -2,6 +2,12 @@
 
 use crate::rnarduino as rn;
 use cty::c_char;
+use cty::c_void;
+use crate::rnarduino::UBaseType_t;
+use crate::rnarduino::TaskHandle_t;
+use crate::rnarduino::TaskFunction_t;
+use alloc::vec::Vec;
+
 
 //--
 pub fn rnDelay(to : u32) -> ()
@@ -39,5 +45,31 @@ pub fn  rnGetTimeUs() -> u32
 {
     unsafe {
         return rn::lnGetUs();
+    }
+}
+
+type rnTaskEntry = fn();
+
+extern "C" fn trampoline( p : *mut cty::c_void)
+{
+    unsafe {
+        let xfunc = *(p as *mut rnTaskEntry);
+        xfunc();
+    }
+}
+
+pub fn rnCreateTask(entry : &rnTaskEntry, name: &str, priority : usize, taskSize : u32)
+{
+    let pointer = *entry as *const ();
+    let name_as_vec :  Vec<char> = name.chars().collect();
+    unsafe {
+        rn::xTaskCreate( 
+            Some(trampoline), //Some(entry) as TaskFunction_t,
+            name_as_vec.as_ptr() as *const cty::c_char, 
+            taskSize as u16, 
+            pointer as *mut c_void ,
+            priority as UBaseType_t, 
+            0 as *mut TaskHandle_t
+        );
     }
 }
