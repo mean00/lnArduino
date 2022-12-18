780e7b68187868a297d5293d9aec2bdb6797c97b
mean <fixounet@free.fr>
2022-12-18 15:55:39 +0100

    FreeRTOS : add task creation binding

--- rust/rnArduino/src/rnOsHelper.rs
+++ rust/rnArduino/src/rnOsHelper.rs
@@ -7,6 +7,7 @@ use crate::rnarduino::UBaseType_t;
 use crate::rnarduino::TaskHandle_t;
 use crate::rnarduino::TaskFunction_t;
 use alloc::vec::Vec;
+use alloc::boxed::Box;
 
 
 //--
@@ -48,26 +49,43 @@ pub fn  rnGetTimeUs() -> u32
     }
 }
 
-type rnTaskEntry = fn();
+pub type rnTaskEntry = fn(param : * mut c_void);
+
+struct TrampolineStruct
+{
+    pub entry : &'static rnTaskEntry,
+    pub param : *mut c_void,
+
+}
 
 extern "C" fn trampoline( p : *mut cty::c_void)
 {
     unsafe {
-        let xfunc = *(p as *mut rnTaskEntry);
-        xfunc();
+        let xparam = p as *mut TrampolineStruct;
+
+        let function_ptr = *((*xparam).entry);
+        function_ptr((*xparam).param);
     }
+    // MEAMLEAK HERE!
+
 }
 
-pub fn rnCreateTask(entry : &rnTaskEntry, name: &str, priority : usize, taskSize : u32)
+pub fn rnCreateTask(function_entry : &'static rnTaskEntry, name: &str, priority : usize, taskSize : u32, param : *mut c_void )
 {
-    let pointer = *entry as *const ();
+    let task_param : TrampolineStruct = TrampolineStruct
+    {
+        entry : function_entry,
+        param : param
+    };    
+    let param_box = Box::new(task_param);
     let name_as_vec :  Vec<char> = name.chars().collect();
+
     unsafe {
         rn::xTaskCreate( 
             Some(trampoline), //Some(entry) as TaskFunction_t,
             name_as_vec.as_ptr() as *const cty::c_char, 
             taskSize as u16, 
-            pointer as *mut c_void ,
+            Box::<TrampolineStruct>::into_raw(param_box) as *mut c_void ,
             priority as UBaseType_t, 
             0 as *mut TaskHandle_t
         );
