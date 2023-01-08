8e54cf34f6d350dd2d297e7a34a19f0a1468879f
mean <fixounet@free.fr>
2023-01-08 17:46:07 +0100

    wch: remove / simplfy wch headers

--- riscv_ch32v3x/core_riscv.h
+++ riscv_ch32v3x/core_riscv.h
@@ -13,22 +13,6 @@
  extern "C" {
 #endif
 
-#define     __I     volatile                /* defines 'read only' permissions */
-#define     __IO    volatile                  /* defines 'read / write' permissions */
-
-/* memory mapped structure for SysTick */
-typedef struct
-{
-    volatile uint32_t CTLR;
-    volatile uint32_t SR;
-    volatile uint64_t CNT;
-    volatile uint64_t CMP;
-}SysTick_Type;
-#define SysTick         ((SysTick_Type *) 0xE000F000)
-
-extern void NVIC_SetPendingIRQ(IRQn_Type);
-extern void NVIC_EnableIRQ(IRQn_Type IRQn);
-extern void NVIC_SetPriority(IRQn_Type IRQn, uint8_t priority);
 
 #ifdef __cplusplus
  };
