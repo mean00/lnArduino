12bf4705f64e6728f63ce1a24021e232d0878197
mean <fixounet@free.fr>
2023-01-25 06:37:20 +0100

    ch32: update demo project

--- demoProject/hello/ch32/platformConfig.cmake
+++ demoProject/hello/ch32/platformConfig.cmake
@@ -7,24 +7,30 @@ IF("${LN_ARCH}" STREQUAL "RISCV") # RISCV
     #SET(PLATFORM_PREFIX riscv-none-embed-) # MRS toolchain
     IF(WIN32)    
       SET(PLATFORM_TOOLCHAIN_PATH todo_todo) # Use /c/foo or c:\foo depending if you use mingw cmake or win32 cmake
-    ELSE(WIN32)
-      
-      #SET(PLATFORM_PREFIX "riscv-none-embed-") # MRS toolchain
-      #SET(PLATFORM_TOOLCHAIN_TRIPLET "riscv-none-embed-")
-      SET(PLATFORM_TOOLCHAIN_PATH "/riscv/tools2/bin")    
-      SET(PLATFORM_C_FLAGS "-march=rv32imac -mabi=ilp32 ")
-      SET(PLATFORM_PREFIX "riscv64-unknown-elf-") # MRS toolchain      
+    ELSE(WIN32)      
+      #--- GCC ---------
+      SET(PLATFORM_TOOLCHAIN_PATH "/riscv/tools2/bin" CACHE INTERNAL "")    
+      IF(USE_HW_FPU)
+        SET(PLATFORM_C_FLAGS "-march=rv32imafc -mabi=ilp32f " CACHE INTERNAL "")
+      ELSE()
+        SET(PLATFORM_C_FLAGS "-march=rv32imac -mabi=ilp32 " CACHE INTERNAL "")
+      ENDIF()
+      SET(PLATFORM_PREFIX "riscv64-unknown-elf-" CACHE INTERNAL "") 
       # FOR CLANG  
       
-      SET(PLATFORM_TOOLCHAIN_TRIPLET "riscv64-unknown-elf-")      
+      SET(PLATFORM_TOOLCHAIN_TRIPLET "riscv64-unknown-elf-" CACHE INTERNAL "")      
       #-- CLANG --
-      SET(PLATFORM_CLANG_C_FLAGS "--target=riscv32 -march=rv32imac -mabi=ilp32 ")      
-      SET(PLATFORM_CLANG_PATH  "/riscv/tools_llvm/bin")
+      # No FPU
+      SET(PLATFORM_CLANG_PATH  "/riscv/tools_llvm/bin" CACHE INTERNAL "")
       SET(PLATFORM_CLANG_VERSION "-16")
-      SET(PLATFORM_CLANG_SYSROOT "/riscv/tools_llvm/lib/clang-runtimes/riscv32-none-eabi/riscv32/")
-      #SET(PLATFORM_CLANG_VERSION "-14")
-      #SET(PLATFORM_CLANG_SYSROOT "/riscv/llvm_xpack_14/")
-
+      IF(USE_HW_FPU)
+          SET(PLATFORM_CLANG_SYSROOT "${PLATFORM_CLANG_PATH}/../lib/clang-runtimes/riscv32-none-eabi/riscv32_hard_fp/" CACHE INTERNAL "")
+          SET(PLATFORM_CLANG_C_FLAGS "--target=riscv32 -march=rv32imafc -mabi=ilp32f  " CACHE INTERNAL "")      
+          SET(PLATFORM_CLANG_EXTRA_LD_ARG "-L${PLATFORM_CLANG_SYSROOT}/lib/rv32imafc/ilp32f" CACHE INTERNAL "")
+      ELSE()
+          SET(PLATFORM_CLANG_SYSROOT "${PLATFORM_CLANG_PATH}/../lib/clang-runtimes/riscv32-none-eabi/riscv32_soft_nofp/" CACHE INTERNAL "")
+          SET(PLATFORM_CLANG_C_FLAGS "--target=riscv32 -march=rv32imac -mabi=ilp32  " CACHE INTERNAL "")      
+      ENDIF()      
     ENDIF(WIN32)
   ELSE( ${LN_MCU} STREQUAL "CH32V3x") #----------- GD32VF103
     SET(PLATFORM_PREFIX riscv32-unknown-elf-)
