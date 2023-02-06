6df558600f6cf568b0b476791bbc0e8aa1868d12
mean <fixounet@free.fr>
2023-02-06 11:04:22 +0100

    rust : remove feature(default_alloc_error_handler)

--- rust/rnArduino/src/lib.rs
+++ rust/rnArduino/src/lib.rs
@@ -2,7 +2,6 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-#![feature(default_alloc_error_handler)]
 #![feature(lang_items)]
 use core::alloc::{GlobalAlloc, Layout};
 extern crate alloc;
