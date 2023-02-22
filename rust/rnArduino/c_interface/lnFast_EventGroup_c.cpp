9385417cdf9b8659c299aa0f576d6d7f6fe68500
mean <fixounet@free.fr>
2023-02-22 07:06:47 +0100

    rust: rewrite fast_event_group in a more usable way as it is by nature thread safe

---
+++ rust/rnArduino/c_interface/lnFast_EventGroup_c.cpp
@@ -0,0 +1,35 @@
+
+#include "lnArduino.h"
+extern "C"
+{
+    #include "lnFast_EventGroup_c.h"
+}
+
+#define WRAPME(x) ((lnFastEventGroup *)x)
+
+lnfast_event_group_c *   lnfast_event_group_create()
+{
+    return (lnfast_event_group_c *)new lnFastEventGroup;
+}
+void  lnfast_event_group_delete(lnfast_event_group_c *f)
+{
+    delete WRAPME(f);
+}
+
+void lnfast_event_group_takeOwnership(const lnfast_event_group_c *inst)
+{
+    WRAPME(inst)->takeOwnership();
+}
+void lnfast_event_group_set_events(const lnfast_event_group_c *inst, uint32_t event)
+{
+    WRAPME(inst)->setEvents(event);
+}
+uint32_t lnfast_event_group_wait_events(const lnfast_event_group_c *inst, uint32_t mask, int32_t timeout)
+{
+    return WRAPME(inst)->waitEvents(mask, timeout);
+}
+uint32_t lnfast_event_group_read_events(const lnfast_event_group_c *inst, uint32_t mask)
+{
+    return WRAPME(inst)->readEvents(mask);
+}
+// EOF
---
+++ rust/rnArduino/c_interface/lnFast_EventGroup_c.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "stdint.h"
+
+// it's not really const
+// but it's thread safe by design
+
+typedef struct 
+{
+    void *dummy;
+}lnfast_event_group_c;
+
+lnfast_event_group_c *   lnfast_event_group_create();
+void        lnfast_event_group_delete(const lnfast_event_group_c *);
+
+void        lnfast_event_group_takeOwnership(const lnfast_event_group_c *inst);
+void        lnfast_event_group_set_events(const lnfast_event_group_c *inst, uint32_t event);
+uint32_t    lnfast_event_group_wait_events(const lnfast_event_group_c *inst, uint32_t mask, int32_t timeout);
+uint32_t    lnfast_event_group_read_events(const lnfast_event_group_c *inst, uint32_t mask);
+   
+// EOF
