b2f49f5d9ddbf1996ac22d66b88ae99786565a5c
mean <fixounet@free.fr>
2023-03-30 15:36:51 +0200

    demo: cdc, dont do blocking call in the callback, it blocks tinyusb

--- demoProject/usb_cdc/hello.cpp
+++ demoProject/usb_cdc/hello.cpp
@@ -9,6 +9,8 @@
 void setup()
 {
     pinMode(LED, OUTPUT);
+    pinMode(PB8, OUTPUT);
+    pinMode(PB9, OUTPUT);
 }
 #define MEVENT(x)                                                                                                      \
     case lnUsbStack::USB_##x: Logger(#x); break;
@@ -25,40 +27,20 @@ void helloUsbEvent(void *cookie, lnUsbStack::lnUsbStackEvents event)
     }
 }
 lnUsbCDC *cdc=NULL;
+uint8_t buffer[100];
+lnFastEventGroup *event_group;
 void cdcEventHandler(void *cookie, int interface,lnUsbCDC::lnUsbCDCEvents event,uint32_t payload)
 {
-  uint8_t buffer[32];
-
-    switch (event)
-    {
-      case lnUsbCDC::CDC_SET_SPEED:
-            Logger("CDC SET SPEED\n");
-            break;
-      case lnUsbCDC::CDC_DATA_AVAILABLE:
-        {
-          int n=cdc->read(buffer,32);
-          if(n)
-          {
-            cdc->write((uint8_t *)">",1);
-            cdc->write(buffer,n);
-          }
-        }
-          break;
-      case lnUsbCDC::CDC_SESSION_START:
-          Logger("CDC SESSION START\n");
-          break;
-      case lnUsbCDC::CDC_SESSION_END:
-          Logger("CDC SESSION END\n");
-          break;
-      default:
-          xAssert(0);
-          break;
-    }
+    event_group->setEvents( 1 << event);
 }
 
 void loop()
 {
     Logger("Starting lnTUSB Test\n");
+
+    event_group= new lnFastEventGroup();
+    event_group->takeOwnership();
+
     lnUsbStack *usb = new lnUsbStack;
     usb->init(5, descriptor);
     usb->setConfiguration(desc_hs_configuration, desc_fs_configuration, &desc_device, &desc_device_qualifier);
@@ -69,8 +51,39 @@ void loop()
     usb->start();
     while (1)
     {
-        Logger("*\n");
-        delay(1000);
-        lnDigitalToggle(LED);
+        uint32_t t = event_group->waitEvents(0xffffffff, 1000);
+        if(!t)
+        {
+            lnDigitalToggle(LED);
+            lnDigitalToggle(PB8);
+            lnDigitalToggle(PB9);
+            continue;
+        }
+        if( t & (1<<lnUsbCDC::CDC_SESSION_START))
+        {
+             Logger("CDC SESSION START\n");
+        }
+        if( t & (1<<lnUsbCDC::CDC_SESSION_END))
+        {
+             Logger("CDC SESSION END\n");
+        }
+        if( t & (1<<lnUsbCDC::CDC_SET_SPEED))
+        {
+            Logger("CDC SET SPEED\n");
+        }
+        if( t & (1<<lnUsbCDC::CDC_DATA_AVAILABLE))
+        {
+             Logger("Dt\n");
+            int n=cdc->read(buffer,100);
+            if(n)
+            {
+                cdc->write((uint8_t *)">",1);
+                cdc->write(buffer,n);
+                cdc->flush();
+            }
+        }
+        
     }
 }
+// EOF
+ 
