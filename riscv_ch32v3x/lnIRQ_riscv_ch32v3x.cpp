b6597b8678ada88a165bf21a2612c0260013ddfe
mean <fixounet@free.fr>
2023-04-24 07:02:34 +0200

    flash : split on per target implementatio, enable enhanced mode for CH32vx

--- riscv_ch32v3x/lnIRQ_riscv_ch32v3x.cpp
+++ riscv_ch32v3x/lnIRQ_riscv_ch32v3x.cpp
@@ -447,15 +447,41 @@ WEAK_INTERRUPT(OTG_FS_IRQHandler)
 void lnSoftSystemReset()
 {
 }
-extern "C" int main();
-extern "C" void __libc_init_array(void);
-extern "C" void   __attribute__((noreturn))  start_c()
+
+extern "C"
 {
+
+extern const char _data, _edata;
+extern const char __bss_start, _end;
+extern const char _data_lma;
+
+int main();
+void __libc_init_array(void);
+void   __attribute__((noreturn))  start_c()
+{
+
+    volatile uint32_t *src = (volatile uint32_t *)&_data_lma;
+    volatile uint32_t *dst = (volatile uint32_t*)&_data;
+    volatile uint32_t *end = (volatile uint32_t*)&_edata;
+
+    while (dst < end) 
+    {
+            *dst++ = *src++;
+    }
+    
+    /* Zero .bss. this will corrupt stack & locals on it*/
+    volatile uint32_t *zstart = (volatile uint32_t*)&__bss_start;
+    volatile uint32_t *zend =   (volatile uint32_t*)&_end;
+    while (zstart < zend) 
+    {
+        *zstart++ = 0;
+    }
     __libc_init_array(); // call ctor before jumping in the code
     main();
     xAssert(0);  
 }
 
+}
 // EOF
 
 
