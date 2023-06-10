9bf1357085aff5fc1916ecea2cfbff752bc3d222
mean <fixounet@free.fr>
2023-06-10 20:55:06 +0200

    autobuild : generate compilation file

--- demoProject/common_CMakeLists.cmake
+++ demoProject/common_CMakeLists.cmake
@@ -3,5 +3,7 @@ SET(AF_FOLDER  ${CMAKE_SOURCE_DIR}/lnArduino/)
 
 include(../common_mcuSelect.cmake)
 SET(CMAKE_TOOLCHAIN_FILE ${AF_FOLDER}/lnArduino.cmake)
+SET(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
 
         
