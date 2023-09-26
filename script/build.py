365fa11300d9f725820948c2b8cc53a68c3b3282
mean <fixounet@free.fr>
2023-09-26 19:47:37 +0200

    non reg: fix build for usb + RP/CH32

--- script/build.py
+++ script/build.py
@@ -5,6 +5,10 @@ import os
 projects = []
 blacklist = ['cmake','everything','usb','hello']
 failures = []
+
+def dbg(c):
+  pass
+
 def grab_demo_folders():
     for item in os.scandir('../demoProject'):
         if item.is_dir():
@@ -16,11 +20,13 @@ def grab_demo_folders():
 def exec_makethingie(title, cmds):
     res=0
     print("\t"+title+"...")
+    dbg(str(cmds))
+    dbg(os.getcwd())
     try:
         res=0 # Assume it taises an excception if a problem occurs
         subprocess.check_call(cmds,stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL) #, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except Exception as e:
-        print("\t OS ERROR")
+        print("\t OS ERROR" +str(e))
         res=-1
     if res!=0:
         print("\t"+title+"FAILED")
@@ -28,7 +34,7 @@ def exec_makethingie(title, cmds):
     print("\t\tok")
     return True
 ##
-def build_single(arch, mcu, proj,extra,extra_title):
+def build_single(arch, mcu,use, proj,extra,extra_title):
     cwd=os.getcwd()
     os.chdir(cwd+'/../demoProject/'+str(proj))
     #print(os.getcwd())
@@ -40,7 +46,7 @@ def build_single(arch, mcu, proj,extra,extra_title):
     subprocess.check_call(['mkdir',build_dir])
     os.chdir(cwd+'/../demoProject/'+str(proj)+'/'+build_dir)
     #
-    cmakeParam= [ 'cmake', '-G', 'Ninja','-DLN_ARCH='+arch,'-DLN_MCU='+mcu,extra,'..']
+    cmakeParam= [ 'cmake', '-G', 'Ninja','-DUSE_'+use+'=True',extra,'..']
     makeParam = ['ninja','-v']
     if exec_makethingie("CMAKE",cmakeParam)==False:
         return False
@@ -48,13 +54,13 @@ def build_single(arch, mcu, proj,extra,extra_title):
         return False
     return True
     #os.chdir(cwd)
-    #print(str(projects))
-def build_all(arch, mcu,extra, extra_title):
+    print(str(projects))
+def build_all(arch, mcu,use, extra, extra_title):
     cwd=os.getcwd()
     for i in projects:
         print("[",arch,"/",mcu,"/",extra_title,"]:",i)
         os.chdir(cwd)
-        if False==build_single(arch,mcu,i,extra,extra_title):
+        if False==build_single(arch,mcu,use,i,extra,extra_title):
             failures.append( str(arch)+'_'+str(mcu)+'_'+str(i)+str(extra_title) )
     os.chdir(cwd)
     pass
@@ -62,12 +68,12 @@ def build_all(arch, mcu,extra, extra_title):
 print("-- Build all --")
 grab_demo_folders()
 with_clang = "-DUSE_CLANG=True"
-build_all("ARM","M3","","gcc")
-build_all("ARM","M3",with_clang,"clang")
-build_all("ARM","M4","","gcc")
-build_all("ARM","M3",with_clang,"clang")
-build_all("RISCV","CH32V3x","","gcc")
-build_all("RISCV","CH32V3x",with_clang,"clang")
+build_all("ARM","M3","","","gcc")
+build_all("ARM","M3","",with_clang,"clang")
+build_all("ARM","M4","GD32F3","","gcc")
+build_all("ARM","M4","GD32F3",with_clang,"clang")
+build_all("RISCV","CH32V3x","CH32V3x","","gcc")
+build_all("RISCV","CH32V3x","CH32V3x",with_clang,"clang")
 # Obsolete build_all("RISCV","VF103")
 print("-- Failures --")
 for i in failures:
