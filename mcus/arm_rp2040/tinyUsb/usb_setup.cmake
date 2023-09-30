d1b68d8f9647f46c40e04e590e130fde98142b5d
mean <fixounet@free.fr>
2023-09-30 16:33:27 +0200

    rp2040: continue cleaning up sdk ipath

--- mcus/arm_rp2040/tinyUsb/usb_setup.cmake
+++ mcus/arm_rp2040/tinyUsb/usb_setup.cmake
@@ -5,27 +5,21 @@ SET(DRIVERS             ${RP2040_FOLDER}/rp2040_usb.cpp  ${RP2040_FOLDER}/lnUSBD
 SET(LN_OPT_TUSB_MCU     OPT_MCU_RP2040)
 SET(LN_OPT_MODE         OPT_MODE_FULL_SPEED)  
 
+
+FOREACH( subdir hardware_uart hardware_gpio pico_stdio pico_platform hardware_timer hardware_resets hardware_irq hardware_base   )
+        LIST(APPEND idir  ${PICO_SDK_PATH}/src/rp2_common/${subdir}/include)
+ENDFOREACH()        
+
 FOREACH( subdir 
-                    src/host/hardware_uart/include/hardware/
-                    src/rp2_common/hardware_uart/include/ 
-                    src/rp2_common/hardware_gpio/include/ 
-                    src/common/pico_time/include/
-                    src/rp2_common/pico_stdio/include/ 
-                    src/rp2_common/pico_platform/include/ 
                     / 
-                    src/common/pico_base/include/ 
-                    src/rp2_common/hardware_timer/include/ 
-                    src/rp2_common/hardware_resets/include/ 
-                    src/rp2_common/hardware_irq/include/ 
+                    src/common/pico_time/include/
+                    src/common/pico_base/include/                     
                     src/common/pico_stdlib/include/pico/
-                    src/boards/include/boards/  
-                    src/rp2040/hardware_structs/include/ 
-                    src/rp2_common/hardware_base/include/ 
+                    src/rp2040/hardware_structs/include/                     
                     src/rp2040/hardware_regs/include
+                    src/boards/include/boards/  
         )
         LIST(APPEND idir  ${PICO_SDK_PATH}/${subdir})
-
 ENDFOREACH()        
 
 SET(LN_TUSB_EXTRA_IDIR ${idir} )
-#SET(LN_TUSB_EXTRA_DEF   "__force_inline=__always_inline")
