aabdebc6180a41eedd8d91d5bb902b47b323fe76
mean <fixounet@free.fr>
2023-12-02 18:04:14 +0100

    uart: begin rx/tx dma

--- mcus/common_bluepill/lnSerialCore.cpp
+++ mcus/common_bluepill/lnSerialCore.cpp
@@ -193,10 +193,10 @@ IRQHANDLER(1)
 IRQHANDLER(2)
 IRQHANDLER(3)
 
-#include "lnSerialRxTx.cpp"
-#include "lnSerialRxTxDma.cpp"
 #include "lnSerialTxOnly.cpp"
 #include "lnSerialTxOnlyDma.cpp"
+#include "lnSerialRxTx.cpp"
+#include "lnSerialRxTxDma.cpp"
 /**
  * @brief Create a Ln Serial Tx Only object
  *
