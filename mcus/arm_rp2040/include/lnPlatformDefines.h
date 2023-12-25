bc809c51f4c88a34e511f949a7fb15a66ac6bad7
mean <fixounet@free.fr>
2023-12-25 14:01:57 +0100

    rp2040 : prepare for code execution in ram

--- mcus/arm_rp2040/include/lnPlatformDefines.h
+++ mcus/arm_rp2040/include/lnPlatformDefines.h
@@ -1,4 +1,5 @@
 
 
 #pragma once
-#define LN_FAST_CODE __attribute__((section(".copy_to_ram.")))
+// #define LN_FAST_CODE __attribute__((section(".copy_to_ram.")))
+#define LN_FAST_CODE
--- mcus/arm_rp2040/toolchain_clang.cmake
+++ mcus/arm_rp2040/toolchain_clang.cmake
@@ -133,7 +133,7 @@ SET(CLANG_LINKER_OPT "${MINI_SYSROOT}/lib/libclang_rt.builtins.a" CACHE INTERNAL
 # --sysroot=${LN_CLANG_SYSROOT}  
 #
 SET(SB2 ${CMAKE_BINARY_DIR}/lnArduino/mcus/arm_rp2040/src/CMakeFiles/rplib.dir/__/conf/bs2_default_padded_checksummed.S.obj)
-SET(CRT ${CMAKE_BINARY_DIR}/lnArduino/mcus/arm_rp2040/src/CMakeFiles/rplib.dir/pico/pico-sdk/src/rp2_common/pico_standard_link/crt0.S.obj)
+SET(CRT ${CMAKE_BINARY_DIR}/lnArduino/mcus/arm_rp2040/src/CMakeFiles/rplib.dir/crt0.S.obj)
 #
 set(CMAKE_CXX_LINK_EXECUTABLE    "<CMAKE_LINKER>  <CMAKE_CXX_LINK_FLAGS>  <LINK_FLAGS> ${LN_LTO}    -Wl,--start-group ${CRT} ${SB2} <OBJECTS>  <LINK_LIBRARIES>  -Wl,--end-group  -Wl,-Map,<TARGET>.map   -o <TARGET> ${GD32_LD_FLAGS} ${GD32_LD_LIBS}  ${CLANG_LINKER_OPT}" CACHE INTERNAL "")
 SET(CMAKE_EXECUTABLE_SUFFIX_C .elf CACHE INTERNAL "")
