fbbd8bc336d4e4214c8c06bfc23d2e1dc789045e
mean <fixounet@free.fr>
2023-12-27 18:16:30 +0100

    rp: ws2812 update demo

--- libraries/WS2812_rp_single/example/gen_sin.py
+++ libraries/WS2812_rp_single/example/gen_sin.py
@@ -1,10 +1,10 @@
 import math
 
-print("const uint8_t sin[180]={")
-for i in range(0,180):
-    ang = i*3.1415/180
-    s=math.cos(ang)*127
-    s=255-int(s)-128
+print("const uint8_t sin[181]={")
+for i in range(0,181): # from 0 to 180 (included)
+    ang = i*math.pi/180.
+    s=(math.cos(ang)*128.)+0.5
+    s=255-int(s)-127
     print(str(s),end="")
     print(",",end="")
     if (i+1)%16==15:
