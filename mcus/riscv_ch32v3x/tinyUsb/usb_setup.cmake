9826533712e74ef6716fa8e2087214146266e1ff
mean <fixounet@free.fr>
2024-02-19 20:32:29 +0100

    merge fs patch

--- mcus/riscv_ch32v3x/tinyUsb/usb_setup.cmake
+++ mcus/riscv_ch32v3x/tinyUsb/usb_setup.cmake
@@ -1,4 +1,5 @@
 SET(CH32V3x_FOLDER ${LN_MCU_FOLDER}/tinyUsb/)
+SET(TINY_FOLDER    ${AF_FOLDER}/libraries/tinyUsb/src/src/portable/wch)
 #SET(LN_OPT_MODE         OPT_MODE_HIGH_SPEED)
 #ADD_DEFINITIONS(-DCFG_TUD_MAX_SPEED=OPT_MODE_HIGH_SPEED -DLN_USB_SPEED=TUSB_SPEED_HIGH -DTUD_OPT_HIGH_SPEED=1)        
 SET(LN_OPT_MODE         OPT_MODE_FULL_SPEED)
@@ -6,8 +7,18 @@ SET(LN_OPT_TUSB_MCU     OPT_MCU_CH32V307)
 INCLUDE_DIRECTORIES(    ${CH32V3x_FOLDER}  )
 
 IF( USE_CH32v3x_USB_HS ) 
-    SET(DRIVERS              ${LNSRC}/lnUsbStack.cpp ${CH32V3x_FOLDER}/dcd_usbhs.cpp        ${CH32V3x_FOLDER}/lnUsbStubs.cpp)
+    SET(DRIVERS              
+                    ${LNSRC}/lnUsbStack.cpp
+                    ${CH32V3x_FOLDER}/dcd_usbhs.cpp
+                    ${CH32V3x_FOLDER}/lnUsbStubs.cpp
+                    ${CH32V3x_FOLDER}/dcd_usbhs_platform.cpp
+            )
 ELSE( )
-    SET(DRIVERS              ${LNSRC}/lnUsbStack.cpp  ${CH32V3x_FOLDER}/dcd_usbfs.c  ${CH32V3x_FOLDER}/dcd_usbfs_platform.cpp     ${CH32V3x_FOLDER}/lnUsbStubs.cpp)
+    SET(DRIVERS     ${LNSRC}/lnUsbStack.cpp
+                    #${CH32V3x_FOLDER}/dcd_usbfs.c  
+                    ${TINY_FOLDER}/dcd_ch32_usbfs.c
+                    ${CH32V3x_FOLDER}/dcd_usbfs_platform.cpp
+                    ${CH32V3x_FOLDER}/lnUsbStubs.cpp
+            )
 ENDIF()        
 
