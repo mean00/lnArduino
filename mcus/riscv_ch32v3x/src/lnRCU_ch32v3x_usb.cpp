ff2663c89572aea786a31fbd0bb3a5496f82a6b6
mean <fixounet@free.fr>
2024-02-25 20:22:16 +0100

    ch32: cleanup usb hs clock

--- mcus/riscv_ch32v3x/src/lnRCU_ch32v3x_usb.cpp
+++ mcus/riscv_ch32v3x/src/lnRCU_ch32v3x_usb.cpp
@@ -1,62 +1,38 @@
-/*
- *  (C) 2021 MEAN00 fixounet@free.fr
- *  See license file
+/**
+ * @file lnRCU_ch32v3x_usb.cpp
+ * @author 2021/2024 MEAN00 
+ * @brief USB HS clock management
+ * 
+ * @copyright Copyright (c) 2021/2024
+ * 
  */
-
 #include "lnArduino.h"
-#include "lnCpuID.h"
-#include "lnPeripheral_priv.h"
 #include "lnRCU.h"
 #include "lnRCU_priv.h"
 extern LN_RCU *arcu;
+
+#define CH32V3_USBHS_PLL_SRC_HSI (0<<27)
+#define CH32V3_USBHS_PLL_SRC_HSE (1<<27) // the datasheet is wrong
+#define CH32V3_USBHS_PREFIX(x)    ((x)<<24)
+#define CH32V3_USBHS_CKREF_SEL(x)    ((x)<<28)
+#define CH32V3_USBHS_CKREF_SRC_USB_PHY    (1<<31)
+#define CH32V3_USBHS_CKREF_SRC_PLL        (0<<31)
+#define CH32V3_USBHS_USB_PHY_ENABLED      (1<<30)
+#define CH32V3_USBHS_USB_PHY_DISABLED     (0<<30)
+
 /**
-    This is the USB HS clock for CH32V3x chip
+    \brief This is the USB HS clock for CH32V3x chip
+    NB: The CFG1 register is called RCC_CFGR2 in the datasheet
 */
 void lnPeripherals::enableUsbHS48Mhz_ch32v3x()
 {
-#if 0
-  RCC_USBCLK48MConfig(RCC_USBCLK48MCLKSource_USBPHY);
-  RCC_USBHSPLLCLKConfig(RCC_HSBHSPLLCLKSource_HSE);
-  RCC_USBHSConfig(RCC_USBPLL_Div2);
-  RCC_USBHSPLLCKREFCLKConfig(RCC_USBHSPLLCKREFCLK_4M);
-  RCC_USBHSPHYPLLALIVEcmd(ENABLE);
-#endif
-#if 1 // def LN_CH32V3X_USE_EXTERNAL_USB_PHY
-
-    uint32_t cfg1 = arcu->CFG1;
-    cfg1 &= 0xffffff;
-    cfg1 |= 1 << 27;   // src = HSE
-    cfg1 |= (1 << 24); // div = 2
-    cfg1 |= (1 << 28); // Ref= 4Mhz
-    cfg1 |= (1 << 31); // Src=USB PHY
-    cfg1 |= (1 << 30); // USB PHY PLL ALive
-    arcu->CFG1 = cfg1;
-
-#else
-
-    int scaler = (2 * lnPeripherals::getClock(pSYSCLOCK)) / 48000000;
-    int x = 0;
-    switch (scaler)
-    {
-    case 2:
-        x = 0;
-        break; // 48 Mhz
-    case 4:
-        x = 1;
-        break; // 96 Mhz
-    case 6:
-        x = 2;
-        break; // 144 Mhz
-    default:
-        xAssert(0); // invalid sys clock
-        break;
-    }
-
-    uint32_t cfg0 = arcu->CFG0;
-    cfg0 &= LN_RCU_CFG0_USBPSC_MASK;
-    cfg0 |= LN_RCU_CFG0_USBPSC(x);
-    arcu->CFG0 = cfg0;
-
-#endif
+    uint32_t cfgr2 = arcu->CFG1;
+    cfgr2 &= 0xffffff;
+    cfgr2 |= CH32V3_USBHS_PLL_SRC_HSE;   // src = HSE
+    cfgr2 |= CH32V3_USBHS_PREFIX(1); // 0= no div, 1=div by 2
+    cfgr2 |= CH32V3_USBHS_CKREF_SEL(1);  // Ref= 4Mhz
+    cfgr2 |= CH32V3_USBHS_CKREF_SRC_USB_PHY; // Src=USB PHY
+    cfgr2 |= CH32V3_USBHS_USB_PHY_ENABLED; // USB PHY PLL ALive
+    arcu->CFG1 = cfgr2;
 }
 // EOF
