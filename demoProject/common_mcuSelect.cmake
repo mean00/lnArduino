627985c770cae2fa4ff477f2395b902a6b6187bc
mean <fixounet@free.fr>
2024-03-24 16:27:54 +0100

    demo: update common mcu select, add rp2040

--- demoProject/common_mcuSelect.cmake
+++ demoProject/common_mcuSelect.cmake
@@ -26,8 +26,17 @@ IF(NOT DEFINED LN_ARCH)
         SET(LN_MCU_SPEED 96000000    CACHE INTERNAL "") #=> ok
         #      SET(LN_MCU_SPEED  72000000    CACHE INTERNAL "") #=> ok
         SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
-        SET(LN_ENABLE_USBD True CACHE INTERNAL "")
         SET(LN_BOOTLOADER_SIZE 8     CACHE INTERNAL "")
+    ELSEIF(USE_RP2040)
+        SET(LN_ARCH            "ARM" CACHE INTERNAL "")
+        SET(LN_MCU             "RP2040"  CACHE INTERNAL "")
+        SET(LN_MCU_RAM_SIZE    255    CACHE INTERNAL "")
+        SET(LN_MCU_SPEED       72000000       CACHE INTERNAL "")
+        SET(LN_MCU_STATIC_RAM  10     CACHE INTERNAL "")
+        SET(LN_MCU_FLASH_SIZE  2048   CACHE INTERNAL "")
+        SET(LN_MCU_EEPROM_SIZE 2     CACHE INTERNAL "")
+        SET(LN_BOOTLOADER_SIZE 8     CACHE INTERNAL "")        
+        SET(LN_SPEC            "picolibc"   CACHE INTERNAL "") # if not set we use nano
     ELSE()
         SET(LN_ARCH            "ARM" CACHE INTERNAL "")
         SET(LN_MCU             "M3"  CACHE INTERNAL "")
@@ -42,7 +51,6 @@ IF(NOT DEFINED LN_ARCH)
           SET(LN_MCU_FLASH_SIZE  128   CACHE INTERNAL "")
           SET(LN_MCU_EEPROM_SIZE 2     CACHE INTERNAL "")
           SET(LN_BOOTLOADER_SIZE 8     CACHE INTERNAL "")
-          SET(LN_ENABLE_USBD True CACHE INTERNAL "")
         ENDIF()
         SET(LN_SPEC         "picolibc"   CACHE INTERNAL "") # if not set we use nano
     ENDIF() # GD32F3
