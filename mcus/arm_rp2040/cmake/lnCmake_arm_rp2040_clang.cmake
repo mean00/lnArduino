8bf0f4e1c952c8ff96d8aa4cd33841a1691abf5e
mean <fixounet@free.fr>
2024-04-27 19:52:30 +0200

    rp2040 : prepare for no flash

--- mcus/arm_rp2040/cmake/lnCmake_arm_rp2040_clang.cmake
+++ mcus/arm_rp2040/cmake/lnCmake_arm_rp2040_clang.cmake
@@ -2,7 +2,12 @@ include(FindPython3)
 MACRO(GENERATE_GD32_FIRMWARE target)
 
   #MESSAGE(STATUS " *** LN_MCU_FOLDER ${LN_MCU_FOLDER}")
-    configure_file( "${LN_MCU_FOLDER}/boards/${GD32_BOARD}/rp2040_linker.ld.in" "${CMAKE_BINARY_DIR}/linker_script.ld" @ONLY)
+    OPTION(RP2040_NO_FLASH "store everything in ram" OFF)
+    IF(RP2040_NO_FLASH)
+        configure_file( "${LN_MCU_FOLDER}/boards/${GD32_BOARD}/rp2040_linker_ram.ld.in" "${CMAKE_BINARY_DIR}/linker_script.ld" @ONLY)
+    ELSE()
+        configure_file( "${LN_MCU_FOLDER}/boards/${GD32_BOARD}/rp2040_linker.ld.in" "${CMAKE_BINARY_DIR}/linker_script.ld" @ONLY)
+    ENDIF()
     ADD_EXECUTABLE(${target} ${ARGN}  ) 
     TARGET_LINK_LIBRARIES(${target} rplib)
     TARGET_LINK_LIBRARIES(${target} ${USED_LIBS} lnArduino) # duplicates are NOT a mistake !
@@ -28,18 +33,20 @@ MACRO(GENERATE_GD32_FIRMWARE target)
                    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                    COMMENT "Memory summary"
     )
-
-    find_program ( elf2uf2      NAMES elf2uf2)    
-    if( "x${elf2uf2}" STREQUAL "xelf2uf2-NOTFOUND")
-            MESSAGE(WARNING "elf2uf2 not found, it is part of the pico SDK")
-    else()
-     add_custom_command(TARGET ${target}
-        POST_BUILD                
-        COMMAND elf2uf2  $<TARGET_FILE:${target}> $<TARGET_FILE:${target}>.uf2
-        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        COMMENT "Generating uf2 files"        
-        )
-    endif()
+    IF(RP2040_NO_FLASH)
+    ELSE()
+        find_program ( elf2uf2      NAMES elf2uf2)    
+        if( "x${elf2uf2}" STREQUAL "xelf2uf2-NOTFOUND")
+                MESSAGE(WARNING "elf2uf2 not found, it is part of the pico SDK")
+        else()
+        add_custom_command(TARGET ${target}
+            POST_BUILD                
+            COMMAND elf2uf2  $<TARGET_FILE:${target}> $<TARGET_FILE:${target}>.uf2
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+            COMMENT "Generating uf2 files"        
+            )
+        endif()
+    ENDIF()
 ENDMACRO(GENERATE_GD32_FIRMWARE target)
 
 MACRO(USE_LIBRARY lib)
