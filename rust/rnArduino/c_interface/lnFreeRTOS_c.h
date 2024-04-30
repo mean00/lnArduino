6d225fc1c82075c06c3de444c9541a85de5ce56e
mean <fixounet@free.fr>
2024-04-30 08:01:01 +0200

    update rust binding

---
+++ rust/rnArduino/c_interface/lnFreeRTOS_c.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "lnFreeRTOS.h"
+#include "stdbool.h"
+#include "stdint.h"
+
+void Logger_chars(int n, const char *data);
---
+++ rust/rnArduino/c_interface/lnSPI_c.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "lnSPI_defines.h"
+#include "stdbool.h"
+#include "stdint.h"
+
+typedef struct // opaque
+{
+    void *dummy;
+} ln_spi_c;
+
+ln_spi_c *lnspi_create(int instance, int pinCs);
+void lnspi_delete(ln_spi_c *i2c);
+//--
+void lnspi_begin(ln_spi_c *instance, uint32_t dataSize);
+void lnspi_end(ln_spi_c *instance);
+void lnspi_set_bit_order(ln_spi_c *instance, spiBitOrder order);
+void lnspi_set_data_mode(ln_spi_c *instance, spiDataMode datamode);
+void lnspi_set_speed(ln_spi_c *instance, uint32_t speed);
+void lnspi_set_ssel(ln_spi_c *instance, int ssel);
+void lnspi_set(ln_spi_c *instance, const lnSPISettings &st);
+
+bool lnspi_write8(ln_spi_c *instance, uint8_t data);
+bool lnspi_write16(ln_spi_c *instance, uint16_t data);
+bool lnspi_wait_for_completion(ln_spi_c *instance, uint16_t data);
+
+bool lnspi_block_write16_repeat(ln_spi_c *instance, unsigned int nbWord, const uint16_t data);
+bool lnspi_block_write8_repeat(ln_spi_c *instance, unsigned int nbByte, const uint8_t data);
+bool lnspi_block_write16(ln_spi_c *instance, unsigned int nbWord, const uint16_t *data);
+bool lnspi_block_write8(ln_spi_c *instance, unsigned int nbByte, const uint8_t *data);
+
+bool lnspi_asyncWrite8(ln_spi_c *instance, int nbBytes, const uint8_t *data, lnSpiCallback *cb, void *cookie,
+                       bool repeat);
+bool lnspi_nextWrite8(ln_spi_c *instance, int nbBytes, const uint8_t *data, lnSpiCallback *cb, void *cookie,
+                      bool repeat);
+bool lnspi_asyncWrite16(ln_spi_c *instance, int nbWords, const uint16_t *data, lnSpiCallback *cb, void *cookie,
+                        bool repeat);
+bool lnspi_nextWrite16(ln_spi_c *instance, int nbWords, const uint16_t *data, lnSpiCallback *cb, void *cookie,
+                       bool repeat);
+bool lnspi_finishAsyncDma(ln_spi_c *instance);
+bool lnspi_waitForAsync(ln_spi_c *instance);
+
+// EOF
