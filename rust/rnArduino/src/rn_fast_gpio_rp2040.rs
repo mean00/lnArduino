712a56aaf5f397ebc51a7e93eb7fb2ded8975a7a
mean <fixounet@free.fr>
2024-05-03 19:25:29 +0200

    Rust: add an indirection level for GPIO definition

---
+++ rust/rnArduino/src/rn_fast_gpio_rp2040.rs
@@ -0,0 +1,51 @@
+#![allow(dead_code)]
+
+use crate::rn_gpio_rp2040_c ;
+//
+use gpio::lnPin as rnPin;
+//
+use gpio::lnGpioMode as rnGpioMode;
+use gpio::lnGetGpioToggleRegister;
+use crate::rn_gpio_rp2040_c as gpio;
+use crate::rn_gpio::pinMode;
+pub struct rnFastIO
+{
+    onoff_adr       : *mut u32,
+    on_bit          : u32,
+    off_bit         : u32
+}
+
+impl rnFastIO
+{
+    pub fn new( pin : rnPin) -> Self
+    {
+        let bit : u32 = 1_u32 <<( (pin as u32)& 0x0f);
+        pinMode(pin,rnGpioMode::lnOUTPUT);
+        let r : rnFastIO ;
+        unsafe {
+            r= rnFastIO {
+                onoff_adr   : lnGetGpioToggleRegister( ((pin as usize ) >> 4) as i32),
+                on_bit      : bit,
+                off_bit     : bit<<16,
+            };
+        }
+        r
+    }
+    #[inline(always)]
+    pub fn on(&mut self)
+    {
+        unsafe {
+            *self.onoff_adr = self.on_bit;
+        }
+    }
+    #[inline(always)]
+    pub fn off(&mut self)
+    {
+        unsafe {
+            *self.onoff_adr = self.off_bit;
+        }
+
+    }
+
+}
+
