25214e2de1eac15f41d65c93f9841c870d349b98
mean <fixounet@free.fr>
2024-11-02 20:22:51 +0100

    ch32 : switch to tinyusb native driver

--- mcus/riscv_ch32v3x/tinyUsb/new/dcd_ch32_usbfs.c
+++ mcus/riscv_ch32v3x/tinyUsb/new/dcd_ch32_usbfs.c
@@ -35,63 +35,7 @@
 #include "device/dcd.h"
 
 // MEANX
-#include "ch32v30x_usb.h"
-
-/* USBOTG_FS Registers */
-typedef struct
-{
-    uint8_t BASE_CTRL;
-    uint8_t UDEV_CTRL;
-    uint8_t INT_EN;
-    uint8_t DEV_ADDR;
-    uint8_t Reserve0;
-    uint8_t MIS_ST;
-    uint8_t INT_FG;
-    uint8_t INT_ST;
-    uint16_t RX_LEN;
-    uint16_t Reserve1;
-    uint8_t UEP4_1_MOD;
-    uint8_t UEP2_3_MOD;
-    uint8_t UEP5_6_MOD;
-    uint8_t UEP7_MOD;
-    uint32_t UEP0_DMA;
-    uint32_t UEP1_DMA;
-    uint32_t UEP2_DMA;
-    uint32_t UEP3_DMA;
-    uint32_t UEP4_DMA;
-    uint32_t UEP5_DMA;
-    uint32_t UEP6_DMA;
-    uint32_t UEP7_DMA;
-    uint16_t UEP0_TX_LEN;
-    uint8_t UEP0_TX_CTRL;
-    uint8_t UEP0_RX_CTRL;
-    uint16_t UEP1_TX_LEN;
-    uint8_t UEP1_TX_CTRL;
-    uint8_t UEP1_RX_CTRL;
-    uint16_t UEP2_TX_LEN;
-    uint8_t UEP2_TX_CTRL;
-    uint8_t UEP2_RX_CTRL;
-    uint16_t UEP3_TX_LEN;
-    uint8_t UEP3_TX_CTRL;
-    uint8_t UEP3_RX_CTRL;
-    uint16_t UEP4_TX_LEN;
-    uint8_t UEP4_TX_CTRL;
-    uint8_t UEP4_RX_CTRL;
-    uint16_t UEP5_TX_LEN;
-    uint8_t UEP5_TX_CTRL;
-    uint8_t UEP5_RX_CTRL;
-    uint16_t UEP6_TX_LEN;
-    uint8_t UEP6_TX_CTRL;
-    uint8_t UEP6_RX_CTRL;
-    uint16_t UEP7_TX_LEN;
-    uint8_t UEP7_TX_CTRL;
-    uint8_t UEP7_RX_CTRL;
-    uint32_t Reserve2;
-    uint32_t OTG_CR;
-    uint32_t OTG_SR;
-} USBFSD_TypeDefx;
-typedef volatile USBFSD_TypeDefx USBFSD_TypeDef;
-#define USBOTG_FS ((USBFSD_TypeDef *)0)
+#include "fs_bridge.c"
 
 /* private defines */
 #define EP_MAX (8)
@@ -209,6 +153,8 @@ static void update_out(uint8_t rhport, uint8_t ep, size_t rx_len)
 /* public functions */
 void dcd_init(uint8_t rhport)
 {
+    dcd_fs_platform_init();   // MEANX get a clock
+    dcd_fs_platform_delay(1); // MEANX let it power up
     // init registers
     USBOTG_FS->BASE_CTRL = USBFS_CTRL_SYS_CTRL | USBFS_CTRL_INT_BUSY | USBFS_CTRL_DMA_EN;
     USBOTG_FS->UDEV_CTRL = USBFS_UDEV_CTRL_PD_DIS | USBFS_UDEV_CTRL_PORT_EN;
