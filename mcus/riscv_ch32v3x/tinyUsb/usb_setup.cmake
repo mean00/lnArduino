cd3943497a1b920fe2bca66f62b8bb9bc537ff03
mean <fixounet@free.fr>
2024-11-02 20:38:22 +0100

    ch32: remove old tinyusb driver (mine!)

--- mcus/riscv_ch32v3x/tinyUsb/usb_setup.cmake
+++ mcus/riscv_ch32v3x/tinyUsb/usb_setup.cmake
@@ -5,43 +5,20 @@ ENDIF()
 SET(LN_OPT_MODE         OPT_MODE_FULL_SPEED)
 SET(LN_OPT_TUSB_MCU     OPT_MCU_CH32V307)
 
-IF(FALSE) # Old driver
-  SET(CH32V3x_FOLDER ${LN_MCU_FOLDER}/tinyUsb/old)
-  INCLUDE_DIRECTORIES(    ${CH32V3x_FOLDER}  )
+SET(CH32V3x_FOLDER ${LN_MCU_FOLDER}/tinyUsb/new)
+INCLUDE_DIRECTORIES(    ${CH32V3x_FOLDER}  )
 
-  IF( USE_CH32v3x_USB_HS )
-    SET(DRIVERS
-                        ${LNSRC}/lnUsbStack.cpp
-                        ${TINY_FOLDER}/dcd_ch32_usbhs.c
-                        ${CH32V3x_FOLDER}/lnUsbStubs.cpp
-                        ${CH32V3x_FOLDER}/dcd_usbhs_platform.cpp
-                )
-  ELSE( )
-    SET(DRIVERS     ${LNSRC}/lnUsbStack.cpp
-                        #${CH32V3x_FOLDER}/dcd_usbfs.c
-                        ${CH32V3x_FOLDER}/dcd_ch32_usbfs.c
-                        ${CH32V3x_FOLDER}/dcd_usbfs_platform.cpp
-                        ${CH32V3x_FOLDER}/../lnUsbStubs.cpp
-                )
-  ENDIF()
-
-ELSE() # New driver
-  SET(CH32V3x_FOLDER ${LN_MCU_FOLDER}/tinyUsb/new)
-  INCLUDE_DIRECTORIES(    ${CH32V3x_FOLDER}  )
-
-  IF( USE_CH32v3x_USB_HS )
-    SET(DRIVERS
-                        ${LNSRC}/lnUsbStack.cpp
-                        ${TINY_FOLDER}/dcd_ch32_usbhs.c
-                        ${CH32V3x_FOLDER}/lnUsbStubs.cpp
-                        ${CH32V3x_FOLDER}/dcd_usbhs_platform.cpp
-                )
-  ELSE( )
-    SET(DRIVERS     ${LNSRC}/lnUsbStack.cpp
-                        ${CH32V3x_FOLDER}/dcd_ch32_usbfs.c
-                        ${CH32V3x_FOLDER}/dcd_usbfs_platform.cpp
-                        ${CH32V3x_FOLDER}/../lnUsbStubs.cpp
-                )
-  ENDIF()
-
+IF( USE_CH32v3x_USB_HS )
+  SET(DRIVERS
+                      ${LNSRC}/lnUsbStack.cpp
+                      ${TINY_FOLDER}/dcd_ch32_usbhs.c
+                      ${CH32V3x_FOLDER}/lnUsbStubs.cpp
+                      ${CH32V3x_FOLDER}/dcd_usbhs_platform.cpp
+              )
+ELSE( )
+  SET(DRIVERS     ${LNSRC}/lnUsbStack.cpp
+                      ${CH32V3x_FOLDER}/dcd_ch32_usbfs.c
+                      ${CH32V3x_FOLDER}/dcd_usbfs_platform.cpp
+                      ${CH32V3x_FOLDER}/../lnUsbStubs.cpp
+              )
 ENDIF()
