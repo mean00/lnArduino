97442685f6c1bcb6779f083c9c530acb978d765c
mean <fixounet@free.fr>
2024-11-09 07:36:50 +0100

    ch32v3 : revert to old usb driver + implement stall from the new driver

---
+++ mcus/riscv_ch32v3x/tinyUsb/old/dcd_usbfs_platform.cpp
@@ -0,0 +1,39 @@
+
+#include "lnArduino.h"
+/**
+ */
+extern "C"
+{
+
+    void dcd_int_enable(uint8_t rhport)
+    {
+        (void)rhport;
+        lnEnableInterrupt(LN_IRQ_OTG_FS);
+    }
+    /**
+     */
+    void dcd_int_disable(uint8_t rhport)
+    {
+        (void)rhport;
+        lnDisableInterrupt(LN_IRQ_OTG_FS);
+    }
+
+    /**
+     */
+    void dcd_int_handler(uint8_t port);
+
+    void OTG_FS_IRQHandler() LN_INTERRUPT_TYPE;
+    void OTG_FS_IRQHandler()
+    {
+        dcd_int_handler(0);
+    }
+    void dcd_fs_platform_init()
+    {
+        lnPeripherals::enable(pUSBFS_OTG_CH32v3x);
+    }
+    void dcd_fs_platform_delay(int ms)
+    {
+        xDelay(ms);
+    }
+}
+// EOF
--- mcus/riscv_ch32v3x/tinyUsb/usb_setup.cmake
+++ mcus/riscv_ch32v3x/tinyUsb/usb_setup.cmake
@@ -1,24 +1,47 @@
 IF(USE_CH32v3x_HW_IRQ_STACK)
-  ADD_DEFINITIONS("-DUSE_CH32v3x_HW_IRQ_STACK")
+    ADD_DEFINITIONS("-DUSE_CH32v3x_HW_IRQ_STACK")
 ENDIF()
 
 SET(LN_OPT_MODE         OPT_MODE_FULL_SPEED)
 SET(LN_OPT_TUSB_MCU     OPT_MCU_CH32V307)
+    
+IF(TRUE) # Old driver
+    SET(CH32V3x_FOLDER ${LN_MCU_FOLDER}/tinyUsb/old)    
+    INCLUDE_DIRECTORIES(    ${CH32V3x_FOLDER}  )
 
-SET(CH32V3x_FOLDER ${LN_MCU_FOLDER}/tinyUsb/new)
-INCLUDE_DIRECTORIES(    ${CH32V3x_FOLDER}  )
+    IF( USE_CH32v3x_USB_HS ) 
+        SET(DRIVERS              
+                        ${LNSRC}/lnUsbStack.cpp
+                        ${TINY_FOLDER}/dcd_ch32_usbhs.c
+                        ${CH32V3x_FOLDER}/lnUsbStubs.cpp
+                        ${CH32V3x_FOLDER}/dcd_usbhs_platform.cpp
+                )
+    ELSE( )
+        SET(DRIVERS     ${LNSRC}/lnUsbStack.cpp
+                        #${CH32V3x_FOLDER}/dcd_usbfs.c  
+                        ${CH32V3x_FOLDER}/dcd_ch32_usbfs.c
+                        ${CH32V3x_FOLDER}/dcd_usbfs_platform.cpp
+                        ${CH32V3x_FOLDER}/../lnUsbStubs.cpp
+                )
+    ENDIF()        
 
-IF( USE_CH32v3x_USB_HS )
-  SET(DRIVERS
-                      ${LNSRC}/lnUsbStack.cpp
-                      ${TINY_FOLDER}/dcd_ch32_usbhs.c
-                      ${CH32V3x_FOLDER}/lnUsbStubs.cpp
-                      ${CH32V3x_FOLDER}/dcd_usbhs_platform.cpp
-              )
-ELSE( )
-  SET(DRIVERS     ${LNSRC}/lnUsbStack.cpp
-                      ${CH32V3x_FOLDER}/dcd_ch32_usbfs.c
-                      ${CH32V3x_FOLDER}/dcd_usbfs_platform.cpp
-                      ${CH32V3x_FOLDER}/../lnUsbStubs.cpp
-              )
+ELSE() # New driver
+    SET(CH32V3x_FOLDER ${LN_MCU_FOLDER}/tinyUsb/new)
+    INCLUDE_DIRECTORIES(    ${CH32V3x_FOLDER}  )
+
+    IF( USE_CH32v3x_USB_HS ) 
+        SET(DRIVERS              
+                        ${LNSRC}/lnUsbStack.cpp
+                        ${TINY_FOLDER}/dcd_ch32_usbhs.c
+                        ${CH32V3x_FOLDER}/lnUsbStubs.cpp
+                        ${CH32V3x_FOLDER}/dcd_usbhs_platform.cpp
+                )
+    ELSE( )
+        SET(DRIVERS     ${LNSRC}/lnUsbStack.cpp                        
+                        ${CH32V3x_FOLDER}/dcd_ch32_usbfs.c
+                        ${CH32V3x_FOLDER}/dcd_usbfs_platform.cpp
+                        ${CH32V3x_FOLDER}/../lnUsbStubs.cpp
+                )
+    ENDIF()        
+
 ENDIF()
