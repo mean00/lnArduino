fb7e3d85b04e5e497cfbc43e2e50dbd99c8ccb6e
mean <fixounet@free.fr>
2024-11-17 12:25:00 +0100

    dfu: fixup prototype

--- libraries/tinyUsb/lnSrc/lnUsbDFUrt.cpp
+++ libraries/tinyUsb/lnSrc/lnUsbDFUrt.cpp
@@ -1,25 +1,24 @@
+#include "lnUsbDFUrt.h"
+#include "device/dcd.h"
 #include "lnArduino.h"
 #include "lnUsbStack.h"
 #include "tusb.h"
-#include "device/dcd.h"
-#include "lnUsbDFUrt.h"
 
-
-static lnDFUCbrt *_dfuCbrt=NULL;
+static lnDFUCbrt *_dfuCbrt = NULL;
 /**
- * 
+ *
  */
-void lnUsbDFURT::addDFURTCb(lnDFUCb *cb)
+void lnUsbDFURT::addDFURTCb(lnDFUCbrt *cb)
 {
-        _dfuCbrt=cb;
+    _dfuCbrt = cb;
 }
 /**
- * 
+ *
  */
 extern "C" void tud_dfu_runtime_reboot_to_dfu_cb()
 {
 
-    if(!_dfuCbrt)
+    if (!_dfuCbrt)
     {
         Logger("No DFU RT callback\n");
         return;
