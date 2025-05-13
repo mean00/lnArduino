d9879defa586d5fc36c3b548ca0090946b232296
mean <fixounet@free.fr>
2025-05-13 08:15:19 +0200

    prepare for esp32, move things around to avoid clash with esp version

---
+++ mcus/riscv_esp32/include/lnIRQ_riscv.h
@@ -0,0 +1,15 @@
+/*
+ *  (C) 2021 MEAN00 fixounet@free.fr
+ *  See license file
+ */
+#pragma once
+#include "lnArduino.h"
+#include "lnPeripherals.h"
+
+#define LN_INTERRUPT_TYPE __attribute__((used)) __attribute__((interrupt("IRQ")));
+
+enum LnIRQ : int
+{
+    LN_IRQ_NONE = 0,
+};
+extern bool xPortIsInsideInterrupt();
