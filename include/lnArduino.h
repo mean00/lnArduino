1dfe9158e23e17d50b3522c26eb716364f6e1ef4
mean <fixounet@free.fr>
2025-05-16 21:00:34 +0200

    compatibility with ESP and native environment

--- include/lnArduino.h
+++ include/lnArduino.h
@@ -38,11 +38,7 @@
 #include "systemHelper.h"
 extern "C"
 {
-#include "freertos/FreeRTOS.h"
-#include "freertos/event_groups.h"
-#include "freertos/queue.h"
-#include "freertos/semphr.h"
-#include "freertos/task.h"
+#include "lnFreeRTOS.h"
 #define xTaskCreate NONONO
 }
 
--- lnFreeRTOS.cmake
+++ lnFreeRTOS.cmake
@@ -2,7 +2,7 @@ SET(FOS ${LNARDUINO_ROOT}/FreeRTOS CACHE INTERNAL "")
 include( ${LN_MCU_FOLDER}/freeRTOS_port_${LN_EXT}.cmake)
 
 FOREACH(common   event_groups.c  list.c  queue.c  stream_buffer.c  tasks.c  timers.c)
-    LIST(APPEND CMN ${FOS}/${common})
+  LIST(APPEND CMN ${FOS}/${common})
 ENDFOREACH(common   event_groups.c  list.c  queue.c  stream_buffer.c  tasks.c  timers.c)
 
 ADD_DEFINITIONS("-DLN_FREERTOS_HEAP_SIZE=(${LN_MCU_RAM_SIZE}-${LN_MCU_STATIC_RAM}-1)")
@@ -10,12 +10,16 @@ ADD_DEFINITIONS("-DLN_FREERTOS_HEAP_SIZE=(${LN_MCU_RAM_SIZE}-${LN_MCU_STATIC_RAM
 SET(MEM ${FOS}/portable/MemMang/heap_4.c)
 #
 SET(FOS_SOURCES ${CMN} ${LN_FREERTOS_PORT_SOURCES} ${MEM} CACHE INTERNAL "")
+MESSAGE(STATUS "Using internal freertos with config in ${LNARDUINO_ROOT}/freertos_config for target ${LN_EXT} boards ${GD32_BOARD}")
 #
-ADD_LIBRARY( FreeRTOS STATIC ${FOS_SOURCES})
-target_include_directories(FreeRTOS PUBLIC  ${FOS}/..)
-target_include_directories(FreeRTOS PUBLIC ${FOS}/../${LN_EXT}/include)
-target_include_directories(FreeRTOS PUBLIC ${FOS}/include)
+ADD_LIBRARY( FreeRTOS STATIC)
+target_sources(FreeRTOS PRIVATE ${FOS_SOURCES})
+target_include_directories(FreeRTOS PUBLIC ${LNARDUINO_ROOT}/freertos_config)
 target_include_directories(FreeRTOS PUBLIC ${LN_FREERTOS_PORT})
 target_include_directories(FreeRTOS PUBLIC ${CMAKE_SOURCE_DIR})
-target_include_directories(FreeRTOS PUBLIC ${LN_MCU_FOLDER}/include)
-target_include_directories(FreeRTOS PUBLIC ${LN_MCU_FOLDER}/boards/${GD32_BOARD}/)
+target_include_directories(FreeRTOS PUBLIC ${FOS}/)
+target_include_directories(FreeRTOS PUBLIC ${FOS}/..)
+target_include_directories(FreeRTOS PUBLIC ${FOS}/../${LN_EXT}/include)
+target_include_directories(FreeRTOS PUBLIC ${FOS}/include)
+target_include_directories(FreeRTOS PUBLIC ${LN_MCU_FOLDER}/boards/${GD32_BOARD}/) # needed to get the freertos_tuning.h
+
