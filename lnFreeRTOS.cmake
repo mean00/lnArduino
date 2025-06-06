2131447234e6d5991da1d7fbdc33667407e8766d
mean <fixounet@free.fr>
2025-06-06 20:17:27 +0200

    better lib merging

--- lnFreeRTOS.cmake
+++ lnFreeRTOS.cmake
@@ -12,14 +12,15 @@ SET(MEM ${FOS}/portable/MemMang/heap_4.c)
 SET(FOS_SOURCES ${CMN} ${LN_FREERTOS_PORT_SOURCES} ${MEM} CACHE INTERNAL "")
 MESSAGE(STATUS "Using internal freertos with config in ${ESPRIT_ROOT}/freertos_config for target ${LN_EXT} boards ${LN_BOARD_NAME}")
 #
+target_include_directories(esprit_dev INTERFACE ${ESPRIT_ROOT}/freertos_config)
+target_include_directories(esprit_dev INTERFACE ${LN_FREERTOS_PORT})
+target_include_directories(esprit_dev INTERFACE ${CMAKE_SOURCE_DIR})
+target_include_directories(esprit_dev INTERFACE ${FOS}/)
+target_include_directories(esprit_dev INTERFACE ${FOS}/..)
+target_include_directories(esprit_dev INTERFACE ${FOS}/include)
+#
 ADD_LIBRARY( FreeRTOS STATIC)
 target_sources(FreeRTOS PRIVATE ${FOS_SOURCES})
-target_include_directories(FreeRTOS PUBLIC ${ESPRIT_ROOT}/freertos_config)
-target_include_directories(FreeRTOS PUBLIC ${LN_FREERTOS_PORT})
-target_include_directories(FreeRTOS PUBLIC ${CMAKE_SOURCE_DIR})
-target_include_directories(FreeRTOS PUBLIC ${FOS}/)
-target_include_directories(FreeRTOS PUBLIC ${FOS}/..)
-target_include_directories(FreeRTOS PUBLIC ${FOS}/include)
 target_link_libraries(FreeRTOS PUBLIC esprit_dev)
 #
 
